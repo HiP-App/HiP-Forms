1134d088fbe5d833cc459e22a39ccd2d79c4be25
Nico Weskamp <Nico Weskamp>
2016-05-30 17:16:20 +0200

    Changes default UI to Material Design Added custom xml for different colors Added custom styles.xml to generate a custom Material Design Theme Commented the stuff Button click out, because of an error

--- HiPMobile/HiPMobile.Droid/MainActivity.cs
+++ HiPMobile/HiPMobile.Droid/MainActivity.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// /*
+//  * Copyright (C) 2016 History in Paderborn App - Universität Paderborn
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *      http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
+
+using System;
 using Android.App;
 using Android.OS;
 using Android.Widget;
@@ -6,7 +22,9 @@ using de.upb.hip.mobile.pcl.BusinessLayer.Models;
 using Realms;
 
 namespace de.upb.hip.mobile.droid {
-    [Activity (Label = "HiPMobile.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Theme = "@style/Theme",
+          Label = "HiPMobile.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+
     public class MainActivity : Activity {
 
         protected override void OnCreate (Bundle bundle)
@@ -25,7 +43,7 @@ namespace de.upb.hip.mobile.droid {
 
             button.Click += (sender, args) => {
                 // Testing BusinessEntities
-                Exhibit exhibit = BusinessEntitiyFactory.CreateBusinessObject<Exhibit> ();
+                /*Exhibit exhibit = BusinessEntitiyFactory.CreateBusinessObject<Exhibit> ();
                 button.Text = Realm.GetInstance ().All<Exhibit> ().Count ().ToString();
                 exhibit.Name = "Dom";
                 StringElement category = BusinessEntitiyFactory.CreateBusinessObject<StringElement> ();
@@ -34,7 +52,7 @@ namespace de.upb.hip.mobile.droid {
                 Page page = BusinessEntitiyFactory.CreateBusinessObject<Page> ();
                 AppertizerPage appertizerPage = BusinessEntitiyFactory.CreateBusinessObject<AppertizerPage> ();
                 page.AppertizerPage = appertizerPage;
-                exhibit.Pages.Add (page);
+                exhibit.Pages.Add (page);*/
 
             };
         }
