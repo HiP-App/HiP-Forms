7bcf2072352ddb967fba05fb8bf83f414c044c3e
mkuennek <mkuennek@mail.uni-paderborn.de>
2016-06-13 18:25:18 +0200

    Fields of ExhibitDetailsActivity, added libraries

---
+++ HiPMobile/HiPMobile.Droid/Activities/ExhibitDetailsActivity.cs
@@ -0,0 +1,209 @@
+﻿// Copyright (C) 2016 History in Paderborn App - Universität Paderborn
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//       http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Android.Content;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using de.upb.hip.mobile.droid.fragments.bottomsheetfragment;
+using de.upb.hip.mobile.droid.Helpers;
+using de.upb.hip.mobile.pcl.BusinessLayer.Models;
+
+namespace de.upb.hip.mobile.droid.Activities
+{
+    public class ExhibitDetailsActivity : AppCompatActivity
+    {
+        
+
+        /// <summary>
+        /// The id of the displayed exhibit.
+        /// </summary>
+        private string exhibitId;
+
+        /// <summary>
+        /// The currently displayed exhibit.
+        /// </summary>
+        private Exhibit exhibit;
+
+        /// <summary>
+        /// The index of the currently displayed page.
+        /// </summary>
+        private int currentPageIndex = 0;
+
+        /// <summary>
+        /// Indicates whether the audio action in the toolbar should be shown (true) or not (false).
+        /// </summary>
+        private bool showAudioAction = false;
+
+        /// <summary>
+        /// Indicates whether audio is currently played (true) or not (false)
+        /// </summary>
+        private bool isAudioPlaying = false;
+
+        /// <summary>
+        /// Indicates whether the audio toolbar is currently displayed (true) or not (false)
+        /// </summary>
+        private bool isAudioToolbarHidden = true;
+
+        /// <summary>
+        /// Extras contained in the Intent that started this activity.
+        /// </summary>
+        private Bundle extra;
+
+        /// <summary>
+        /// Stores the current action associated with the FAB.
+        /// </summary>
+        private BottomSheetConfig.FabAction fabAction;
+
+        /// <summary>
+        /// Reference to the BottomSheetFragment currently displayed
+        /// </summary>
+        private BottomSheetFragment bottomSheetFragment = null;
+
+        // logging
+        private static readonly string Tag = "ExhibitDetailsActivity";
+
+        // keys for saving/accessing the state
+        private static readonly string KEY_EXHIBIT_NAME = "ExhibitDetailsActivity.ExhibitId";
+        private static readonly string KEY_CURRENT_PAGE_INDEX = "ExhibitDetailsActivity.currentPageIndex";
+        private static readonly string KEY_AUDIO_PLAYING = "ExhibitDetailsActivity.isAudioPlaying";
+        private static readonly string KEY_AUDIO_TOOLBAR_HIDDEN = "ExhibitDetailsActivity.isAudioToolbarHidden";
+        private static readonly string KEY_EXTRAS = "ExhibitDetailsActivity.extras";
+
+        // ui elements
+        private FloatingActionButton fab;
+        private View bottomSheet;
+        private BottomSheetBehavior bottomSheetBehavior;
+        private LinearLayout mRevealView;
+        private ImageButton btnPlayPause;
+        private ImageButton btnPreviousPage;
+        private ImageButton btnNextPage;
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        public void DisplayCurrenExhibitPage()
+        {
+            
+        }
+
+        public void DisplayNextExhibitPage()
+        {
+            
+        }
+
+        public void DisplayPreviousExhibitPage()
+        {
+            
+        }
+
+        private void UpdateAudioFile()
+        {
+            
+        }
+
+        public void SetFabAction()
+        {
+            
+        }
+
+        private bool ShowAudioToolbar()
+        {
+            return false;
+        }
+
+        private bool HideAudioToolBar()
+        {
+            return false;
+        }
+
+        public override void OnBackPressed()
+        {
+            base.OnBackPressed();
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnPrepareOptionsMenu(IMenu menu)
+        {
+            return base.OnPrepareOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            return base.OnOptionsItemSelected(item);
+        }
+
+        
+
+        private void UpdatePlayPauseButtonIcon()
+        {
+            
+        }
+
+        private void DisplayAudioAction()
+        {
+            
+        }
+
+        private void ShowCaptions()
+        {
+            
+        }
+
+        public void DoBindService()
+        {
+            
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+
+        #region AudioControls
+        private void StartAudioPlayback()
+        {
+
+        }
+
+        private void PauseAudioPlayback()
+        {
+
+        }
+
+        private void StopAudioPlayback()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ HiPMobile/HiPMobile.Droid/Helpers/MediaPlayerService.cs
@@ -0,0 +1,188 @@
+﻿// Copyright (C) 2016 History in Paderborn App - Universität Paderborn
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using de.upb.hip.mobile.pcl.BusinessLayer.Models;
+
+namespace de.upb.hip.mobile.droid.Helpers {
+    /// <summary>
+    /// This class controls playing audio files. As the audio needs to be playable regardless of the screen activity 
+    /// and also while the screen is off, this needs to be a service.The service should be started from one activity
+	///	and then be given to the other activities in which it is used, otherwise a good control is impossible.
+    /// </summary>
+    public class MediaPlayerService : Android.App.Service, MediaPlayer.IOnPreparedListener, MediaPlayer.IOnErrorListener
+    {
+
+        public static readonly string ACTION_PLAY = "de.upb.hip.mobile.PLAY";  //the intentions can probably be erased
+        public static readonly string ACTION_STOP = "de.upb.hip.mobile.STOP";
+
+        bool AudioFileIsSet = false;
+
+        private MediaPlayer mediaPlayer;
+        private IBinder binder;
+
+        private Audio a1 = BusinessEntitiyFactory.CreateBusinessObject<Audio> ();
+
+        public MediaPlayerService ()
+        {
+            binder = new MediaPlayerBinder (this);
+        }
+
+        public override IBinder OnBind (Intent intent)
+        {
+            return binder;
+        }
+
+        public override void OnCreate ()
+        {
+            // is this the onCreate in android?
+            try
+            {
+                //the service is bound, therefore this function is used rather than OnStartCommand
+                //mediaPlayer = MediaPlayer.Create(this, a1.getAudioDir());
+                mediaPlayer.SetAudioStreamType (Stream.Music);
+                mediaPlayer.SetOnPreparedListener (this);
+                mediaPlayer.PrepareAsync ();
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+
+        public int OnStartCommand(Intent intent, int flags, int startId)
+        {
+            try
+            {
+                //            mMediaPlayer = MediaPlayer.create(this, songList[current].getAudioDir());
+                mediaPlayer.SetAudioStreamType(Stream.Music);
+                mediaPlayer.SetOnPreparedListener(this);
+                mediaPlayer.PrepareAsync();
+            }
+            catch (Exception e)
+            {
+                //            add an exception handling
+            }
+
+            return 1; //keeps the service running until told otherwise
+            // 1 = START_STICKY
+        }
+
+        public void OnPrepared (MediaPlayer mp)
+        {
+            //the media player will be prepared when starting an activity,
+            // but that may not be the right time to start the audio
+            //therefore this is empty
+        }
+
+        public bool OnError (MediaPlayer mp, MediaError what, int extra)
+        {
+            return false;
+        }
+
+        //following are the functions for the app to use to control the media player
+        public void StartSound()
+        {
+            if (!AudioFileIsSet)
+            {
+                //mediaPlayer = MediaPlayer.create(this, a1.getAudioDir());
+            }
+            mediaPlayer.Start();
+        }
+
+        public void StopSound()
+        {
+            mediaPlayer.Stop();
+        }
+
+        public void PauseSound()
+        {
+            mediaPlayer.Pause();
+        }
+
+        public void changeAudioFile ()
+        {
+            try
+            {
+                mediaPlayer.Reset ();
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+
+        /** sets a specific audio file*/
+        public void SetAudioFile(Audio audio)
+        {
+            try
+            {
+                mediaPlayer.Stop();
+                mediaPlayer.Reset();
+                //may be needed for handling audio files later. until know, leave this commented in here.
+                //mediaPlayer = MediaPlayer.create(this, audio.getAudioDir());
+                AudioFileIsSet = true;
+            }
+            catch (Exception e)
+            {
+                //            add an exception handling
+            }
+        }
+
+        /** sets a specific audio file*/
+        public void SetAudioFile(int audio)
+        {
+            try
+            {
+                mediaPlayer.Stop();
+                mediaPlayer.Reset();
+                //mediaPlayer = MediaPlayer.create(this, audio);
+                AudioFileIsSet = true;
+            }
+            catch (Exception e)
+            {
+                //            add an exception handling
+            }
+        }
+
+        public bool GetAudioFileIsSet()
+        {
+            //since the mediaplayer is used as a service, in the beginning it can't yet be called
+            //as it is not yet created. however, as soon as the play button is used, the media player
+            //surely is existend. the first time, it is pushed, there is not yet set an audiofile
+            //because it couldn't be set before, so this needs to be checked and an audio file can then
+            //be set if necessary
+            return AudioFileIsSet;
+        }
+
+        public class MediaPlayerBinder : Binder {
+
+            public MediaPlayerBinder (MediaPlayerService service)
+            {
+                this.service = service;
+            }
+
+            private readonly MediaPlayerService service;
+            public MediaPlayerService GetService()
+            {
+                return service;
+            }
+        }
+
+}
+}
