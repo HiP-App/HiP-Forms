684a0947fbf30b5a886536f869b499863b2d12ea
Zlatna Nikolova <zlatnanikolova@gmail.com>
2016-07-06 18:39:40 +0200

    Set auto layout for launch screen. Rearange files structure.

---
+++ HiPMobile/HiPMobile.iOS/ProgressIndicatorViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace HiPMobile.iOS
+{
+    public partial class ProgressIndicatorViewController : UIViewController
+    {
+        public ProgressIndicatorViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
