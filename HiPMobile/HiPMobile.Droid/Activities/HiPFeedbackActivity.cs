d15045a6f042c9b60f73bb7f895572e3b08d68a0
mkiehl <mkiehl@mail.uni-paderborn.de>
2016-09-05 14:13:22 +0200

    Changed action bar color

--- HiPMobile/HiPMobile.Droid/Activities/HiPFeedbackActivity.cs
+++ HiPMobile/HiPMobile.Droid/Activities/HiPFeedbackActivity.cs
@@ -1,3 +1,19 @@
+// /*
+//  * Copyright (C) 2016 History in Paderborn App - Universität Paderborn
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *      http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,8 +21,12 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Support.V4.Content;
 using Android.Views;
 using Android.Widget;
 using HockeyApp;
@@ -19,8 +39,21 @@ namespace de.upb.hip.mobile.droid.Activities
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Toast.MakeText (this, "HiP Feedback Activity", ToastLength.Long).Show ();
-;            // Create your application here
+
+            ActionBar.SetDisplayHomeAsUpEnabled(true);
+            ActionBar.SetBackgroundDrawable (new ColorDrawable(Resources.GetColor (Resource.Color.colorPrimary)));
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    Finish ();
+                    return true;
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
         }
     }
 }
