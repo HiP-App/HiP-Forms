2f3610540b9c8e9ed38765758e52b11acdff89b6
Bastian Brinkmann <babri@mail.uni-paderborn.de>
2016-10-10 15:38:58 +0200

    Size of dots adapts to current resolution Setting "ThumbOffset" for correct position of thumb on the seekbar

--- HiPMobile/HiPMobile.Droid/Helpers/CustomSeekBar.cs
+++ HiPMobile/HiPMobile.Droid/Helpers/CustomSeekBar.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Android.Content;
@@ -19,68 +20,71 @@ using Android.Graphics;
 using Android.Util;
 using Android.Widget;
 
-namespace de.upb.hip.mobile.droid.Helpers {
-    public class CustomSeekBar : SeekBar {
+namespace de.upb.hip.mobile.droid.Helpers
+{
+	public class CustomSeekBar : SeekBar
+	{
+		private Paint selectedColor, unselectedColor;
+		private const int MaxDotHeight = 15;
 
-        private static readonly int HALF_SIZE = 15;
-        private RectF position;
+		public CustomSeekBar(Context context) : base(context)
+		{
+			Init();
+		}
 
-        private Paint selectedColor, unselectedColor;
-        private Paint textPaint;
+		public CustomSeekBar(Context context, IAttributeSet attrs) : base(context, attrs)
+		{
+			Init();
+		}
 
-        public CustomSeekBar (Context context) : base (context)
-        {
-            Init ();
-        }
+		public List<int> DotList { get; set; } = new List<int>();
 
-        public CustomSeekBar (Context context, IAttributeSet attrs) : base (context, attrs)
-        {
-            Init ();
-        }
+		private void Init()
+		{
+			selectedColor = new Paint(PaintFlags.AntiAlias)
+			{
+				Color = Resources.GetColor(Resource.Color.colorPrimary)
+			};
+			selectedColor.SetStyle(Paint.Style.Fill);
 
-        public List<int> DotList { get; set; } = new List<int> ();
+			unselectedColor = new Paint(PaintFlags.AntiAlias)
+			{
+				Color = Resources.GetColor(Resource.Color.textColorSecondaryInverse)
+			};
 
-        private void Init ()
-        {
-            selectedColor = new Paint (PaintFlags.AntiAlias);
-            selectedColor.Color = Resources.GetColor (Resource.Color.colorPrimary);
-            selectedColor.SetStyle (Paint.Style.Fill);
+			selectedColor.SetStyle(Paint.Style.Fill);
+		}
 
-            unselectedColor = new Paint (PaintFlags.AntiAlias);
-            unselectedColor.Color = Resources.GetColor (Resource.Color.textColorSecondaryInverse);
+		protected override void OnDraw(Canvas canvas)
+		{
+			var paddingLeft = PaddingLeft;
+			var paddingRight = PaddingRight;
+			var paddingTop = PaddingTop;
 
-            selectedColor.SetStyle (Paint.Style.Fill);
-            position = new RectF ();
+			float seekBarWidth = canvas.Width - (paddingLeft + paddingRight);
+			var halfOfSeekbarHeight = (canvas.Height + paddingTop) * .5f;
+			int dotRadius = halfOfSeekbarHeight >= MaxDotHeight ? MaxDotHeight
+																	: Convert.ToInt32(halfOfSeekbarHeight);
+			ThumbOffset = dotRadius;
 
-            textPaint = new Paint (PaintFlags.AntiAlias);
-            textPaint.Color = Resources.GetColor (Resource.Color.textColorSecondary);
-            textPaint.TextSize = 48;
-        }
+			for (var i = 0; i < DotList.Count; i++)
+			{
+				var middleOfDot = seekBarWidth * DotList.ElementAt(i) / 100;
+				var positionOfDot = new RectF();
+				positionOfDot.Set(paddingLeft + middleOfDot - dotRadius,
+									halfOfSeekbarHeight - dotRadius,
+									paddingLeft + middleOfDot + dotRadius,
+									halfOfSeekbarHeight + dotRadius);
 
-        protected override void OnDraw (Canvas canvas)
-        {
-            var paddingLeft = PaddingLeft;
-            var paddingTop = PaddingTop;
-            float margin = canvas.Width - (paddingLeft + PaddingRight);
-            var halfHeight = (canvas.Height + paddingTop) * .5f;
+				var progress = DotList.ElementAt(i);
+				canvas.DrawOval(
+					positionOfDot,
+					progress <= Progress ? selectedColor : unselectedColor
+				);
+			}
 
-            for (var i = 0; i < DotList.Count; i++)
-            {
-                var pos = (int) (margin / 100 * DotList.ElementAt (i));
+			base.OnDraw(canvas);
+		}
 
-                position.Set (paddingLeft + pos - HALF_SIZE, halfHeight - HALF_SIZE,
-                              paddingLeft + pos + HALF_SIZE, halfHeight + HALF_SIZE);
-
-                var progress = DotList.ElementAt (i);
-                canvas.DrawOval (
-                    position,
-                    progress < Progress ? selectedColor : unselectedColor
-                    );
-                //            canvas.drawText("653", paddingLeft + pos - HALF_SIZE * 2, halfHeight + HALF_SIZE * 4, mTextPaint);
-            }
-
-            base.OnDraw (canvas);
-        }
-
-    }
+	}
 }
