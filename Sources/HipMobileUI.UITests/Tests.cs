9d88700b8a58cf1e793c9f4958fc7e734a101430
mkuennek <mkuennek@mail.uni-paderborn.de>
2017-01-17 21:36:58 +0100

    Adjusted tests

--- Sources/HipMobileUI.UITests/Tests.cs
+++ Sources/HipMobileUI.UITests/Tests.cs
@@ -14,6 +14,7 @@ namespace HipMobileUI.UITests
             {
                 case Platform.Android:
                     app = ConfigureApp.Android
+                            .DeviceSerial ("169.254.190.187:5555")
                                 .ApkFile(@"..\..\..\HipMobileUI.Droid\bin\Release\de.upb.hip.mobile.droid.forms.apk")
                                 .StartApp();
                     break;
@@ -27,56 +28,46 @@ namespace HipMobileUI.UITests
         }
 
         #region AppLaunches_MainScreenHasTextBlue
-
-        [Test, Category("UITestAndroid")]
-        public void Android_AppLaunches_MainScreenHasTextBlue()
+        [TestCase(Platform.Android), Category("UITestAndroid")]
+        [TestCase(Platform.iOS), Category("UITestiOS")]
+        public void AppLaunches_MainScreenHasTextBlue(Platform platform)
         {
-            BeforeEachTest(Platform.Android);
-            AppLaunches_MainScreenHasTextBlue ();
-        }
+            BeforeEachTest(platform);
 
-        [Test, Category("UITestiOS")]
-        public void IOS_AppLaunches_MainScreenHasTextBlue()
-        {
-            BeforeEachTest(Platform.iOS);
-            AppLaunches_MainScreenHasTextBlue ();
-        }
-
-        private void AppLaunches_MainScreenHasTextBlue()
-        {
-            var result = app.Query (x => x.Text ("Blue"));
-            Assert.Greater (result.Length, 0);
+            var result = app.Query(x => x.Text("Blue"));
+            Assert.Greater(result.Length, 0);
         }
 
         #endregion
 
         #region DummyViewsUiTest
 
-        [Test, Category("UITestAndroid")]
-        public void Android_DummyViewsUiTest()
+        [TestCase(Platform.Android), Category("UITestAndroid")]
+        [TestCase(Platform.iOS), Category("UITestiOS")]
+        public void DummyViewsUiTest(Platform platform)
         {
-            BeforeEachTest(Platform.Android);
-            DummyViewsUiTest ();
-        }
+            BeforeEachTest(platform);
 
-        [Test, Category("UITestiOS")]
-        public void IOS_DummyViewsUiTest()
-        {
-            BeforeEachTest(Platform.iOS);
-            DummyViewsUiTest ();
-        }
-
-
-        private void DummyViewsUiTest()
-        {
-            app.Tap(x => x.Marked("OK"));
+            app.Tap(x => x.Marked(GetPlatformMenuName (platform)));
             app.Tap(x => x.Text("Blue").Index(1));
 
             app.DragCoordinates(0, 500, 500, 500);
             app.Tap(x => x.Text("Red"));
 
-            app.Tap(x => x.Marked("OK"));
+            app.Tap(x => x.Marked(GetPlatformMenuName(platform)));
             app.Tap(x => x.Text("Green"));
+        }
+
+        private string GetPlatformMenuName (Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return "OK";
+            }
+            else
+            {
+                return "Menu";
+            }
         }
 
         #endregion
