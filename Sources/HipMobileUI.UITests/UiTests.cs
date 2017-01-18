154fc9c7d128ef4381ddac22b0adb1849d6535cc
Bastian Brinkmann <babri@mail.uni-paderborn.de>
2017-01-18 13:51:46 +0100

    Cleanup and comments

--- Sources/HipMobileUI.UITests/UiTests.cs
+++ Sources/HipMobileUI.UITests/UiTests.cs
@@ -1,20 +1,42 @@
-﻿using NUnit.Framework;
+﻿// Copyright (C) 2017 History in Paderborn App - Universität Paderborn
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.ComponentModel;
+using NUnit.Framework;
 using Xamarin.UITest;
 
 namespace HipMobileUI.UITests
 {
+    /// <summary>
+    /// Class containg UI tests for the application
+    /// On windows only the android tests can be executed (exactly one android simulator needs to run)
+    /// </summary>
     [TestFixture]
-    public class Tests
+    public class UiTests
     {
         private IApp app;
 
+        /// <summary>
+        /// Sets up the application according to the platform, the test should run on
+        /// </summary>
+        /// <param name="platform"></param>
         private void BeforeEachTest(Platform platform)
         {
             switch (platform)
             {
                 case Platform.Android:
                     app = ConfigureApp.Android
-                            .DeviceSerial ("169.254.190.187:5555")
                                 .ApkFile(@"..\..\..\HipMobileUI.Droid\bin\Release\de.upb.hip.mobile.droid.forms.apk")
                                 .StartApp();
                     break;
@@ -24,10 +46,8 @@ namespace HipMobileUI.UITests
                                 .StartApp ();
                     break;
             }
-            
         }
 
-        #region AppLaunches_MainScreenHasTextBlue
         [TestCase(Platform.Android, Category= "UITestAndroid")]
         [TestCase(Platform.iOS, Category = "UITestiOS")]
         public void AppLaunches_MainScreenHasTextBlue(Platform platform)
@@ -37,10 +57,6 @@ namespace HipMobileUI.UITests
             var result = app.Query(x => x.Text("Blue"));
             Assert.Greater(result.Length, 0);
         }
-
-        #endregion
-
-        #region DummyViewsUiTest
 
         [TestCase(Platform.Android, Category = "UITestAndroid")]
         [TestCase(Platform.iOS, Category = "UITestiOS")]
@@ -60,17 +76,16 @@ namespace HipMobileUI.UITests
 
         private string GetPlatformMenuName (Platform platform)
         {
-            if (platform == Platform.Android)
+            switch (platform)
             {
-                return "OK";
-            }
-            else
-            {
-                return "Menu";
+                case Platform.Android:
+                    return "OK"; //text for the burger menu icon in android
+                case Platform.iOS:
+                    return "Menu";
+                default:
+                    throw new InvalidEnumArgumentException (nameof (platform), (int) platform, typeof (Platform));
             }
         }
-
-        #endregion
 
     }
 }
