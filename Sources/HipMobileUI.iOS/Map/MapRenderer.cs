978a58e0776b62bcebc2d96e47471704df7b4694
Zlatna <zlatnanikolova@gmail.com>
2017-01-20 17:08:40 +0100

    Show and manage user location

--- Sources/HipMobileUI.iOS/Map/MapRenderer.cs
+++ Sources/HipMobileUI.iOS/Map/MapRenderer.cs
@@ -17,6 +17,8 @@ namespace HipMobileUI.iOS.Map
 {
     class MapRenderer : ViewRenderer<OsmMap, MKMapView> {
 
+        private LocationManager locationManager;
+
         protected override void OnElementChanged(ElementChangedEventArgs<OsmMap> e)
         {
             base.OnElementChanged(e);
@@ -27,43 +29,73 @@ namespace HipMobileUI.iOS.Map
                 var mapView = new MKMapView();
                 this.SetNativeControl(mapView);
                 var nativeMap = Control as MKMapView;
-                //nativeMap.ShowsUserLocation = true;
 
                 var overlay = new MKTileOverlay ("http://tile.openstreetmap.org/{z}/{x}/{y}.png") {CanReplaceMapContent = true};
                 mapView.AddOverlay(overlay, MKOverlayLevel.AboveLabels);
                 mapView.OverlayRenderer = (view, mkOverlay) => new MKTileOverlayRenderer(overlay);
-
-                MKCoordinateRegion region = nativeMap.Region;
-                region.Span.LatitudeDelta = 0.02; // TODO - unify with the zoomlevel in AppSharedData
-                region.Span.LongitudeDelta = 0.02;
-                region.Center = new CLLocationCoordinate2D(AppSharedData.PaderbornCenter.Latitude, AppSharedData.PaderbornCenter.Longitude);
-                nativeMap.Region = region;
 
                 if (e.NewElement != null)
                 {
                     InitAnnotations(nativeMap, e.NewElement.ExhibitSet);
                     nativeMap.GetViewForAnnotation = GetViewForAnnotation;
                     nativeMap.CalloutAccessoryControlTapped += OnCalloutAccessoryControlTapped;
+                    nativeMap.DidUpdateUserLocation += OnDidUpdateUserLocation;
                 }
 
                 if (e.OldElement != null)
                 {
                     nativeMap.GetViewForAnnotation = null;
                     nativeMap.CalloutAccessoryControlTapped -= OnCalloutAccessoryControlTapped;
+                    nativeMap.DidUpdateUserLocation -= OnDidUpdateUserLocation;
                 }
+
+                locationManager = new LocationManager();
+                locationManager.StartLocationUpdates();
+                nativeMap.ShowsUserLocation = true;
+
+                nativeMap.ShowsCompass = true;
+                
+                initMapPosition (nativeMap);
+            }
+        }
+        
+      private void initMapPosition (MKMapView mapView)
+        {
+            if (mapView.UserLocation.Coordinate.Equals (new CLLocationCoordinate2D (0, 0)) && CLLocationManager.LocationServicesEnabled)
+            {
+                MKCoordinateRegion region = mapView.Region;
+                var center = new CLLocationCoordinate2D(AppSharedData.PaderbornCenter.Latitude, AppSharedData.PaderbornCenter.Longitude);
+                mapView.SetRegion(MKCoordinateRegion.FromDistance(center, 1000, 1000), true);
             }
         }
 
-        private MKAnnotationView GetViewForAnnotation(MKMapView mapview, IMKAnnotation annotation)
+        private void OnDidUpdateUserLocation (object sender, MKUserLocationEventArgs e)
         {
-            if (annotation is MKUserLocation)
+            var mapView = sender as MKMapView;
+            mapView.CenterCoordinate = e.UserLocation.Coordinate;
+        }
+
+        private MKAnnotationView GetViewForAnnotation(MKMapView mapView, IMKAnnotation annotation)
+        {
+            MKAnnotationView annotationView = null;
+            MKAnnotationView dequedView = null;
+            if (annotation.Coordinate.Equals (mapView.UserLocation.Coordinate) && !(annotation is ExhibitAnnotation)) //(annotation is MKUserLocation) doesn't work
             {
-                return null;
+                const string userAnnotationReusableId = "UserAnnotation";
+                dequedView = mapView.DequeueReusableAnnotation(userAnnotationReusableId);
+                if (dequedView != null)
+                {
+                    return dequedView;
+                }
+                else
+                {
+                    annotationView = new UserAnnotationView (annotation, userAnnotationReusableId);
+                    return annotationView;
+                }
             }
 
             const string annotationReusableId = "ExhibitAnnotation";
-            MKAnnotationView annotationView = null;
-            var dequedView = mapview.DequeueReusableAnnotation(annotationReusableId);
+            dequedView = mapView.DequeueReusableAnnotation(annotationReusableId);
             if (dequedView != null)
             {
                 dequedView.Annotation = annotation;
@@ -86,7 +118,7 @@ namespace HipMobileUI.iOS.Map
             if (annotation != null)
             {
                 var exhibitId = annotation.ExhibitId;
-                // open exhibit details
+                //TODO: exhibit details
             }
         }
 
@@ -97,6 +129,48 @@ namespace HipMobileUI.iOS.Map
                 var annotation = new ExhibitAnnotation(exhibit.Location.Latitude, exhibit.Location.Longitude, exhibit.Id,
                                                                                 exhibit.Name, exhibit.Description);
                 map.AddAnnotation (annotation);
+            }
+        }
+    }
+
+    public class LocationManager : CLLocationManager {
+
+        protected CLLocationManager Manager { get; }
+
+        public LocationManager ()
+        {
+            Manager = new CLLocationManager();
+            // Manager.PausesLocationUpdatesAutomatically = true;
+
+            //In order to use user location background update info.plist -> to <key>UIBackgroundModes</key> uncomment  <!--string>location</string-->
+            // iOS 8 has additional permissions requirements
+            if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                //Manager.RequestAlwaysAuthorization(); // works in background  
+                Manager.RequestWhenInUseAuthorization (); // only in foreground
+            }
+            if (UIDevice.CurrentDevice.CheckSystemVersion(9, 0))
+            {
+                Manager.AllowsBackgroundLocationUpdates = false;
+            }
+        }
+
+        public void StartLocationUpdates()
+        {
+
+            if (CLLocationManager.LocationServicesEnabled)
+            {
+                //TODO: magic numbers
+                Manager.DesiredAccuracy = 100;//meters
+                Manager.DistanceFilter = 100;
+                Manager.StartUpdatingLocation ();
+            }
+            else
+            {
+                {
+                    UIAlertView alert = new UIAlertView("", "The user location cannot be retrieved.", null, "ok", null);
+                    alert.Show ();
+                }
             }
         }
     }
