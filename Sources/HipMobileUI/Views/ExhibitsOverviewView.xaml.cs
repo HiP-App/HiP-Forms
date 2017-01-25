0c82d50d6217bc85f369d83fd9c2652ec453c774
mkuennek <mkuennek@mail.uni-paderborn.de>
2017-01-25 17:57:01 +0100

    Changing layout for landscape mode

--- Sources/HipMobileUI/Views/ExhibitsOverviewView.xaml.cs
+++ Sources/HipMobileUI/Views/ExhibitsOverviewView.xaml.cs
@@ -11,17 +11,68 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+using HipMobileUI.Helpers;
 using HipMobileUI.Navigation;
 using HipMobileUI.ViewModels.Views;
 using Xamarin.Forms;
 
 namespace HipMobileUI.Views
 {
-    public partial class ExhibitsOverviewView : ContentView, IViewFor<ExhibitsOverviewViewModel>
-    {
+    public partial class ExhibitsOverviewView : ContentView, IViewFor<ExhibitsOverviewViewModel> {
+
+        private Orientation orientation;
         public ExhibitsOverviewView()
         {
             InitializeComponent();
+            orientation = Orientation.Undefined;
         }
+
+        protected override void OnSizeAllocated (double width, double height)
+        {
+            base.OnSizeAllocated (width, height);
+
+            if (width <= height)
+            {
+                if (orientation != Orientation.Portrait)
+                {
+                    // portrait mode
+                    this.Grid.RowDefinitions.Clear();
+                    this.Grid.ColumnDefinitions.Clear();
+
+                    this.Grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.5, GridUnitType.Star) });
+                    this.Grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.5, GridUnitType.Star) });
+                    this.Grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+
+                    Grid.SetRow(Map, 0);
+                    Grid.SetRow(List, 1);
+                    Grid.SetColumn(Map, 0);
+                    Grid.SetColumn(List, 0);
+
+                    orientation = Orientation.Portrait;
+                }
+            }
+            else if (width > height)
+            {
+                if (orientation != Orientation.Landscape)
+                {
+                    // landscape mode
+                    this.Grid.RowDefinitions.Clear();
+                    this.Grid.ColumnDefinitions.Clear();
+
+                    this.Grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    this.Grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(0.5, GridUnitType.Star) });
+                    this.Grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(0.5, GridUnitType.Star) });
+
+                    Grid.SetRow(Map, 0);
+                    Grid.SetRow(List, 0);
+                    Grid.SetColumn(Map, 0);
+                    Grid.SetColumn(List, 1);
+
+                    orientation = Orientation.Landscape;
+                }
+            }
+        }
+
     }
 }
