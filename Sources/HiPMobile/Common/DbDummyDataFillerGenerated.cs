cb3209a322d84a65a3076d7e6bb2bc4a2e8d8fc3
Nico Weskamp <weskampj@mail.uni-paderborn.de>
2017-02-03 15:41:24 +0100

    Added Paderquellgebiet

--- Sources/HiPMobile/Common/DbDummyDataFillerGenerated.cs
+++ Sources/HiPMobile/Common/DbDummyDataFillerGenerated.cs
@@ -1,4 +1,4 @@
-﻿/////////////////////////////////////////////////
+/////////////////////////////////////////////////
 // This file has been generated automatically! //
 /////////////////////////////////////////////////
 
@@ -20,107 +20,154 @@
 using de.upb.hip.mobile.pcl.BusinessLayer.Managers;
 using de.upb.hip.mobile.pcl.BusinessLayer.Models;
 
-namespace de.upb.hip.mobile.pcl.Common
-{
-    public partial class DbDummyDataFiller
-    {
+namespace de.upb.hip.mobile.pcl.Common {
+    public partial class DbDummyDataFiller {
 
-        public static int DatabaseVersion { get; } = 90004;
+        public static int DatabaseVersion { get; } = 1;
 
-        public void InsertData()
+        public void InsertData ()
         {
-            using (DbManager.StartTransaction())
+            using (DbManager.StartTransaction ())
             {
-                var sprechertext_1 = CreateAudio(
-    "sprechertext_1.mp3",
-    "Während des bisherigen Rundgangs haben Sie erfahren, wie wichtig das Gebiet zwischen Lippe und Pader für die Politik Karls des Großen ab den 770er Jahren war. Erinnern wir uns nur an die große Reichsversammlung im Jahre 777! Zu diesem Anlass fanden sich Franken, Sachsen, aber auch arabische Gesandte aus Spanien hier in Paderborn zusammen.\nAber was fanden diese Personen hier vor? Wie hat man sich das damalige Paderborn, die sogenannte „urbs Karoli“, eigentlich vorzustellen? Lange Zeit fragten sich Historiker und Archäologen, ob die „urbs Karoli“ tatsächlich in Paderborn existierte und wenn ja, wo genau? <fn> Einen Überblick über die hierzu geäußerten Vermutungen bietet Birgit Mecke: Der Stand der Forschungen vor den Grabungskampagnen Winkelmanns, in: Sveva Gai / Birgit Mecke: Est locus insignis…: Die Pfalz Karls des Großen in Paderborn und ihre bauliche Entwicklung bis zum Jahr 1002. Die Neuauswertung der Ausgrabungen Wilhelm Winkelmanns in den Jahren 1964-1978 (Denkmalpflege und Forschung in Westfalen 40,2), Mainz 2004, Bd. 1, S. 1-8. </fn> Die karolingischen Geschichtswerke sprechen für Paderborn nicht von palatium, dem lateinischen Wort für „Pfalz“. Und es gab auch keine archäologischen Anhaltspunkte für die Pfalz Karls des Großen in Paderborn. Noch Mitte des 20. Jahrhunderts schrieb der Akademieprofessor und Domkapitular Alois Fuchs, dass „für eine [karolingische] Pfalz in Paderborn nicht nur alle urkundlichen Bezeugungen fehlen, sondern auch alle Baureste, die für die charakteristischen Pfalzgebäude, den Reichssaal und die Pfalzkapelle, sprechen könnten <fn>Alois A. Fuchs: Zur Frage der Bautätigkeit des Bischofs Badurad am Paderborner Dom, in: Westfälische Zeitschrift 97 (1947), S. 3-34, hier S. 5. </fn>.“  Sichtbar waren einzig verbaute Überreste der Domburg Bischof Meinwerks aus dem 11. Jahrhundert. Diese Überreste hatten bereits Mitte des 19. Jahrhunderts das Interesse von Lokalforschern geweckt <fn>Vgl. J. B. Johann Bernhard Greve: Der kaiserliche und bischöfliche Palast in Paderborn, in: Blätter zur näheren Kunde Westfalens 6/4 (1868), S. 33-38.</fn>.\nJetzt stehen Sie zwischen dem Dom und dem Museum in der Kaiserpfalz. Dieses große und repräsentative Gebäude mit den Rundbogenfenstern sieht so aus, wie man sich eine Kaiserpfalz vorstellt. Doch handelt es sich dabei um die Pfalz Karls des Großen? Nein! Es ist die archäologische Rekonstruktion der Pfalz Bischof Meinwerks aus dem frühen 11. Jahrhundert. \n Aber wo befand sich nun die karolingische Kaiserpfalz? Sehen Sie die etwa 31 mal 10 m große, rechteckige Fläche zwischen Ihnen und dem Museum? Sie ist durch Bruchsteinmauern abgegrenzt. Das sind die aus konservatorischen Gründen aufgemauerten Fundamente der sog. aula regia, der Königshalle Karls des Großen. Wenn Sie genau hinschauen, sehen Sie ein rotes Ziegelband. Dieses trennt das originale Bruchsteinmauerwerk von später, im Zuge der Rekonstruktion ergänzten Steinen.",
-    ""
-  );
-                var sprechertext_2_1 = CreateAudio(
+                var paderquellgebiet1 = CreateAudio (
+                    "paderquellgebiet1.mp3",
+                    "Hier, im Paderquellgebiet, entspringt in über 200 Quellen die Pader. Sie ist mit etwa vier Kilometern Länge nicht nur der kürzeste Fluss Deutschlands, sondern gab der Stadt auch ihren Namen: Paderborn. Pader ist ein alter germanischer Gewässername. Was genau dieser Name bedeutet hat, als er vor vielen Jahrhunderten entstanden ist, lässt sich heute nicht mehr sagen – man nimmt aber an, dass die sprachliche Form (*PaÞ(e)ra) vermutlich so viel wie „Wasserlauf“ oder „sich einen Weg bahnendes Wasser“ bedeutete. Das Grundwort born kommt aus dem Altsächsischen [Infobutton zur Altsächsischen Sprache]. Damals sprach man noch von brunno, das an unser heutiges „Brunnen“ erinnert, aber die Bedeutung „Wasser“, „Quelle“ oder auch „Ursprung“ hat.",
+                    ""
+                );
+                var paderquellgebiet2 = CreateAudio (
+                    "paderquellgebiet2.mp3",
+                    "-brunnen bzw. seine niederdeutsche Form -born sind typische Grundwörter deutscher Flussnamen. Häufig wurden diese im Mittelalter auch auf Siedlungen übertragen, was wir noch heute in Städtenamen wie Brunn, Heilbronn, Eschborn oder eben Paderborn hören können. Die Zusammensetzung aus Pader und born bezeichnet also eine Siedlung „an den Quellen des Flusses Pader.",
+                    ""
+                );
+                var paderquellgebiet3 = CreateAudio (
+                    "paderquellgebiet3.mp3",
+                    "Im Jahre 777 wird der Name Paderborn in den Geschichtswerken erwähnt. Die Geschichtswerke wurden damals ausschließlich in Latein verfasst. Die Schreiber mussten also mit der lateinischen Schrift volkssprachige Wörter aufschreiben. Das stellte eine besondere Herausforderung dar, denn es gab noch kein Regelwerk wie den Duden. Daher schrieben die Autoren den Siedlungsnamen so, wie sie ihn hörten: Paderbrunnon,  Patresbrun,  Padresbrunnon,  Paderbrunnen  oder Padrabrunno.All das sind Formen des Namens Paderborn, die in karolingischer Zeit in den Geschichtswerken vorkommen. Eine sächsische Siedlung gab es aber auch schon zuvor.  Aber mit Karl dem Großen wurde Paderborn zu einem wichtigen fränkischen Stützpunkt im neueroberten sächsischen Gebiet ausgebaut. Dazu hören Sie mehr auf dem Weg zur Kaiserpfalz.",
+                    ""
+                );
+                var paderquellgebiet4 = CreateAudio (
+                    "paderquellgebiet4.mp3",
+                    "Die naturräumliche Schönheit Paderborns wurde bereits 890 in der Translatio Sancti Liborii gerühmt: Unter allen Orten aber, die zur Errichtung der Hauptkirchen in diesem Gebiet bestimmt wurden, von dem wir sprechen, ragte der bezeichnete Paderborner Bischofssitz durch ein besonderes Ansehen hervor, weil er von allen Seiten eine weite Ebene besitzt, die gleichwohl durch zahlreiche abwechslungsreiche Verschiedenartigkeit an Wäldern ausreichend geziert ist und fruchtbare Ackerböden besitzt, die Früchte jeglicher Art hervorbringen. Noch von der Stadtmauer aus sichtbar liegt linker Hand ein Wald, der viele tausend Schritte in Länge und Breite mißt und sogar für kleines und großes Vieh Weiden besitzt, ja zur Bienenzucht so geeignet ist, daß er jener Gegend zu gleichen scheint, die die hl. Schrift ein Land nennt, das von Milch und Honig fließt. Zur Anmut des Ortes trägt noch bei, daß sich sehr heilsame Quellen innerhalb der Stadt selbst wie zu einem kleinen Fluß von unvergleichlicher Art und Wasserreichheit vereinen. Nirgendwo weit und breit haben wir kaum etwas ähnliches jemals gesehen. Es fehlt hier auch nicht an weiteren Flüssen, die sehr ergiebig den Nutzen bringen, der von ihnen verlangt wird. Das Klima ist dort milder, trotz der kalten Erdregion durch die Gunst der Natur gemäßigter und bringt gewöhnlich sämtliche Früchte schneller zur Reife als in allen anderen Teilen der Region.",
+                    ""
+                );
+                var sprechertext_1 = CreateAudio (
+                    "sprechertext_1.mp3",
+                    "Während des bisherigen Rundgangs haben Sie erfahren, wie wichtig das Gebiet zwischen Lippe und Pader für die Politik Karls des Großen ab den 770er Jahren war. Erinnern wir uns nur an die große Reichsversammlung im Jahre 777! Zu diesem Anlass fanden sich Franken, Sachsen, aber auch arabische Gesandte aus Spanien hier in Paderborn zusammen.\nAber was fanden diese Personen hier vor? Wie hat man sich das damalige Paderborn, die sogenannte „urbs Karoli“, eigentlich vorzustellen? Lange Zeit fragten sich Historiker und Archäologen, ob die „urbs Karoli“ tatsächlich in Paderborn existierte und wenn ja, wo genau? <fn> Einen Überblick über die hierzu geäußerten Vermutungen bietet Birgit Mecke: Der Stand der Forschungen vor den Grabungskampagnen Winkelmanns, in: Sveva Gai / Birgit Mecke: Est locus insignis…: Die Pfalz Karls des Großen in Paderborn und ihre bauliche Entwicklung bis zum Jahr 1002. Die Neuauswertung der Ausgrabungen Wilhelm Winkelmanns in den Jahren 1964-1978 (Denkmalpflege und Forschung in Westfalen 40,2), Mainz 2004, Bd. 1, S. 1-8. </fn> Die karolingischen Geschichtswerke sprechen für Paderborn nicht von palatium, dem lateinischen Wort für „Pfalz“. Und es gab auch keine archäologischen Anhaltspunkte für die Pfalz Karls des Großen in Paderborn. Noch Mitte des 20. Jahrhunderts schrieb der Akademieprofessor und Domkapitular Alois Fuchs, dass „für eine [karolingische] Pfalz in Paderborn nicht nur alle urkundlichen Bezeugungen fehlen, sondern auch alle Baureste, die für die charakteristischen Pfalzgebäude, den Reichssaal und die Pfalzkapelle, sprechen könnten <fn>Alois A. Fuchs: Zur Frage der Bautätigkeit des Bischofs Badurad am Paderborner Dom, in: Westfälische Zeitschrift 97 (1947), S. 3-34, hier S. 5. </fn>.“  Sichtbar waren einzig verbaute Überreste der Domburg Bischof Meinwerks aus dem 11. Jahrhundert. Diese Überreste hatten bereits Mitte des 19. Jahrhunderts das Interesse von Lokalforschern geweckt <fn>Vgl. J. B. Johann Bernhard Greve: Der kaiserliche und bischöfliche Palast in Paderborn, in: Blätter zur näheren Kunde Westfalens 6/4 (1868), S. 33-38.</fn>.\nJetzt stehen Sie zwischen dem Dom und dem Museum in der Kaiserpfalz. Dieses große und repräsentative Gebäude mit den Rundbogenfenstern sieht so aus, wie man sich eine Kaiserpfalz vorstellt. Doch handelt es sich dabei um die Pfalz Karls des Großen? Nein! Es ist die archäologische Rekonstruktion der Pfalz Bischof Meinwerks aus dem frühen 11. Jahrhundert. \n Aber wo befand sich nun die karolingische Kaiserpfalz? Sehen Sie die etwa 31 mal 10 m große, rechteckige Fläche zwischen Ihnen und dem Museum? Sie ist durch Bruchsteinmauern abgegrenzt. Das sind die aus konservatorischen Gründen aufgemauerten Fundamente der sog. aula regia, der Königshalle Karls des Großen. Wenn Sie genau hinschauen, sehen Sie ein rotes Ziegelband. Dieses trennt das originale Bruchsteinmauerwerk von später, im Zuge der Rekonstruktion ergänzten Steinen.",
+                    ""
+                );
+                var sprechertext_2_1 = CreateAudio (
                     "sprechertext_2_1.mp3",
                     "Falls Sie Schwierigkeiten haben sollten, sich in diesem Mauergewirr zurechtzufinden, blicken Sie auf Ihr Display. Hier werden die Mauern der aula regia rot hervorgehoben.",
                     ""
-                  );
-                var sprechertext_2_2 = CreateAudio(
+                );
+                var sprechertext_2_2 = CreateAudio (
                     "sprechertext_2_2.mp3",
                     "Vor hundert Jahren hätten Sie davon noch nichts sehen können. Denn dieses Gelände war damals mit Fachwerkhäusern bebaut, die im 2. Weltkrieg zerstört wurden. Einen Eindruck, wie sich das Gelände nördlich des Doms verändert hat, bietet der Fotoslider auf Ihrem Display. Die Kriegszerstörungen boten aber auch neue Möglichkeiten. So legte man ab den 1950er Jahren Teile der karolingischen Befestigungsmauer frei und konnte so ihren Verlauf rekonstruieren.",
                     ""
-                  );
-                var kaiserPfalzImage = CreateImage(
+                );
+                var kaiserPfalzImage = CreateImage (
                     "Die Pfalz Karls des Großen",
                     "Die Überreste der Kaiserpflaz von Westen aus betrachtet.",
                     "kaiserpfalz_teaser.jpg"
-                  );
-                var kaiserPfalzImage1 = CreateImage(
+                );
+                var kaiserPfalzImage1 = CreateImage (
                     "Die Pfalz Karls des Großen",
                     "Die Überreste der Kaiserpflaz von Westen aus betrachtet.",
                     "kaiserpfalz_image_1.jpg"
-                  );
-                var kaiserpfalzImage2 = CreateImage(
+                );
+                var kaiserpfalzImage2 = CreateImage (
                     "Von Westen aus betrachtet",
                     "Die Überreste der Kaiserpflaz mit rot eingefärbten Mauern der aula regia von Westen aus betrachtet.",
                     "kaiserpfalz_image_2.jpg"
-                  );
-                var kaiserpfalz_Slider1_1 = CreateImage(
+                );
+                var kaiserpfalz_Slider1_1 = CreateImage (
                     "",
                     "Blick von Norden auf die Paderquellen vor 1945",
                     "kaiserpfalz_slider_1_1.jpg"
-                  );
-                var kaiserpfalz_Slider1_2 = CreateImage(
+                );
+                var kaiserpfalz_Slider1_2 = CreateImage (
                     "",
                     "Blick von Norden über die Treppe vor 1945",
                     "kaiserpfalz_slider_1_2.jpg"
-                  );
-                var kaiserpfalz_Slider1_3 = CreateImage(
+                );
+                var kaiserpfalz_Slider1_3 = CreateImage (
                     "",
                     "Blick von Südwesten vor 1935",
                     "kaiserpfalz_slider_1_3.jpg"
-                  );
-                var kaiserpfalz_Slider1_4 = CreateImage(
+                );
+                var kaiserpfalz_Slider1_4 = CreateImage (
                     "",
                     "Blick von Norden vor 1945",
                     "kaiserpfalz_slider_1_4.jpg"
-                  );
-                var kaiserpfalz_Slider1_5 = CreateImage(
+                );
+                var kaiserpfalz_Slider1_5 = CreateImage (
                     "",
                     "Blick von Norden um 1945",
                     "kaiserpfalz_slider_1_5.jpg"
-                  );
-                var theoTeaser = CreateImage(
+                );
+                var noImage = CreateImage (
+                    "No Image",
+                    "Hier fehlt das Bild",
+                    "noImage.jpg"
+                );
+                var quellen1Teaser = CreateImage (
+                    "",
+                    "",
+                    "quellen1Teaser.jpg"
+                );
+                var theoTeaser = CreateImage (
                     null,
                     null,
                     "theo_teaser.jpg"
-                  );
-                var kaiserpfalz = CreateExhibit(
+                );
+                var kaiserpfalz = CreateExhibit (
                     "Die Pfalz Karls des Großen",
                     "",
                     51.7189826,
                     8.754652599999986,
-                    new[] { "Kirche" },
-                    new[] { "Dom" },
+                    new[] {"Kirche"},
+                    new[] {"Dom"},
                     kaiserPfalzImage
-                  );
-                kaiserpfalz.Pages.Add(CreateAppetizerPage("Ein befestigter Stützpunkt in Sachsen – Aufbau und Entwicklung.", kaiserPfalzImage));
-                kaiserpfalz.Pages.Add(CreateImagePage(kaiserPfalzImage1, null, null, sprechertext_1));
-                kaiserpfalz.Pages.Add(CreateImagePage(kaiserpfalzImage2, null, null, sprechertext_2_1));
-                kaiserpfalz.Pages.Add(CreateTimeSliderPage("", "", new long[] { 0, 1, 2, 3, 4 }, new[] { kaiserpfalz_Slider1_1, kaiserpfalz_Slider1_2, kaiserpfalz_Slider1_3, kaiserpfalz_Slider1_4, kaiserpfalz_Slider1_5 }, true, sprechertext_2_2));
-                var karlsrouteSet = DbManager.CreateBusinessObject<ExhibitSet>();
-                karlsrouteSet.ActiveSet.Add(kaiserpfalz);
-                var karlsroute = CreateRoute(
+                );
+                kaiserpfalz.Pages.Add (CreateAppetizerPage ("Ein befestigter Stützpunkt in Sachsen – Aufbau und Entwicklung.", kaiserPfalzImage));
+                kaiserpfalz.Pages.Add (CreateImagePage (kaiserPfalzImage1, null, null, sprechertext_1));
+                kaiserpfalz.Pages.Add (CreateImagePage (kaiserpfalzImage2, null, null, sprechertext_2_1));
+                kaiserpfalz.Pages.Add (CreateTimeSliderPage ("", "", new long[] {0, 1, 2, 3, 4},
+                                                             new[]
+                                                                 {kaiserpfalz_Slider1_1, kaiserpfalz_Slider1_2, kaiserpfalz_Slider1_3, kaiserpfalz_Slider1_4, kaiserpfalz_Slider1_5},
+                                                             true, sprechertext_2_2));
+                var Paderquellgebiet = CreateExhibit (
+                    "Das Paderquellgebiet und der Name „Paderborn“",
+                    "",
+                    51.72178,
+                    8.75087,
+                    new[] {"Natur"},
+                    new[] {"Paderquelle"},
+                    quellen1Teaser
+                );
+                Paderquellgebiet.Pages.Add (CreateAppetizerPage ("Die Siedlung an den 200 Quellen. Woher kommt eigentlich der Name Paderborn", quellen1Teaser));
+                Paderquellgebiet.Pages.Add (CreateImagePage (noImage, null, null, paderquellgebiet1));
+                Paderquellgebiet.Pages.Add (CreateImagePage (noImage, null, null, paderquellgebiet2));
+                Paderquellgebiet.Pages.Add (CreateImagePage (noImage, null, null, paderquellgebiet3));
+                Paderquellgebiet.Pages.Add (CreateImagePage (noImage, null, null, paderquellgebiet4));
+                var karlsrouteSet = DbManager.CreateBusinessObject<ExhibitSet> ();
+                karlsrouteSet.ActiveSet.Add (kaiserpfalz);
+                karlsrouteSet.ActiveSet.Add(Paderquellgebiet);
+                var karlsroute = CreateRoute (
                     "Karlsroute",
                     "Auf der Spur Karls des Großen!",
                     1800,
                     4.2,
                     theoTeaser
-                  );
+                );
 
                 foreach (var exhibit in karlsrouteSet.ActiveSet)
                 {
-                    karlsroute.Waypoints.Add(CreateWayPoint(exhibit));
+                    karlsroute.Waypoints.Add (CreateWayPoint (exhibit));
                 }
 
 
             }
         }
+
     }
 }
