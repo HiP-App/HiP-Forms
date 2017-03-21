aac45876649a68a161e9977d326971cca3a6b652
mkuennek <mkuennek@mail.uni-paderborn.de>
2017-03-21 14:04:49 +0100

    Added timeout for displaying exhibit nearby messages on the main screen

--- Sources/HipMobileUI/Location/NearbyExhibitManager.cs
+++ Sources/HipMobileUI/Location/NearbyExhibitManager.cs
@@ -12,12 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
 using System.Collections.Generic;
+using de.upb.hip.mobile.pcl.BusinessLayer.Managers;
 using de.upb.hip.mobile.pcl.BusinessLayer.Models;
 using de.upb.hip.mobile.pcl.Common;
 using de.upb.hip.mobile.pcl.Helpers;
 using HipMobileUI.Helpers;
 using HipMobileUI.Navigation;
+using HipMobileUI.Resources;
 using HipMobileUI.ViewModels.Pages;
 
 namespace HipMobileUI.Location
@@ -32,26 +35,47 @@ namespace HipMobileUI.Location
         /// </summary>
         /// <param name="exhibits">The exhibits to check</param>
         /// <param name="gpsLocation">The gps location of the user</param>
-        void CheckNearExhibit(IEnumerable<Exhibit> exhibits, GeoLocation gpsLocation);
+        /// <param name="considerTimeouts">Parameter indicating if timeouts for displaying the exhibit nearby message should be taken into account.</param>
+        void CheckNearExhibit(IEnumerable<Exhibit> exhibits, GeoLocation gpsLocation, bool considerTimeouts);
 
     }
 
     public class NearbyExhibitManager : INearbyExhibitManager
     {
-
+        private readonly TimeSpan dialogTimeout = TimeSpan.FromMinutes (20);
         public event ExhibitVisitedDelegate ExhibitVisitedEvent;
 
-        public async void CheckNearExhibit (IEnumerable<Exhibit> exhibits, GeoLocation gpsLocation)
+        public async void CheckNearExhibit (IEnumerable<Exhibit> exhibits, GeoLocation gpsLocation, bool considerTimeouts)
         {
             foreach (Exhibit e in exhibits)
             {
                 var dist = MathUtil.CalculateDistance(e.Location, gpsLocation);
                 if (dist < AppSharedData.ExhibitRadius)
                 {
+                    if (considerTimeouts)
+                    {
+                        DateTimeOffset now = DateTimeOffset.Now;
+                        if (e.LastNearbyTime.HasValue)
+                        {
+                            if (now.Subtract (e.LastNearbyTime.Value) <= dialogTimeout)
+                            {
+                                // dialog for this exhibit was shown recently, skip it
+                                continue;
+                            }
+                        }
+
+                        // update the time the dialog was last shown
+                        using (DbManager.StartTransaction ())
+                        {
+                            e.LastNearbyTime = now;
+                        }
+                    }
+
                     var result =
                         await
-                            IoCManager.Resolve<INavigationService>()
-                                      .DisplayAlert("Sehenwürdigkeit in der Nähe", "Möchten sie sich " + e.Name + " genauer ansehen", "Ja", "Nein");
+                            IoCManager.Resolve<INavigationService> ()
+                                      .DisplayAlert (Strings.ExhibitNearby_ExhibitNearby, Strings.ExhibitNearby_Question_Part1 + " \"" + e.Name + "\" " +Strings.ExhibitNearby_Question_Part2,
+                                                     Strings.ExhibitNearby_Confirm, Strings.ExhibitNearby_Reject);
 
                     if (result)
                     {
