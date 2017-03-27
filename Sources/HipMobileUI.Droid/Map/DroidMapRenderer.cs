e782b5c8981566d4726fcf1742a7dd28a82fdc99
weskampj <weskampj@mail.uni-paderborn.de>
2017-03-27 13:51:06 +0200

    iss-hipm-650 (#59)
    * changed tile server of ios version
    set permission for using unsecure connection to tile server
    change the update time in geoLocator
    
    * fixed zoom problem
    
    * renaming

--- Sources/HipMobileUI.Droid/Map/DroidMapRenderer.cs
+++ Sources/HipMobileUI.Droid/Map/DroidMapRenderer.cs
@@ -45,6 +45,7 @@ using Application = Xamarin.Forms.Application;
 using Org.Osmdroid.Tileprovider;
 
 [assembly: ExportRenderer (typeof (OsmMap), typeof (DroidMapRenderer))]
+
 namespace de.upb.hip.mobile.droid.Map {
     internal class DroidMapRenderer : Xamarin.Forms.Platform.Android.AppCompat.ViewRenderer<OsmMap, MapView>, IMapListener {
 
@@ -79,16 +80,17 @@ namespace de.upb.hip.mobile.droid.Map {
                 activity = Context as Activity;
                 routeCalculator = RouteCalculator.Instance;
                 SetNativeControl (mapView);
-                //for the app testing the tile server was changed to watercolor style and layer was added (only for android)
+
+                //Watercolorstyle map with labels layer
                 mapView.SetTileSource (new XYTileSource ("OSM", null, 0, 18, 256, ".png",
                                                          new[] {"http://c.tile.stamen.com/watercolor/"}));
-               MapTileProviderBasic tileProvider = new MapTileProviderBasic(activity);
-               ITileSource tileSource = new XYTileSource("MyCustomTiles", null, 1, 16, 256, ".png",
-                            new[] { "http://b.sm.mapstack.stamen.com/(watercolor,streets-and-labels)/" });
+                MapTileProviderBasic tileProvider = new MapTileProviderBasic (activity);
+                ITileSource tileSource = new XYTileSource ("MyCustomTiles", null, 1, 16, 256, ".png",
+                                                           new[] {"http://b.sm.mapstack.stamen.com/(watercolor,streets-and-labels)/"});
                 tileProvider.TileSource = (tileSource);
-                TilesOverlay tilesOverlay = new TilesOverlay(tileProvider, activity.BaseContext);
+                TilesOverlay tilesOverlay = new TilesOverlay (tileProvider, activity.BaseContext);
                 tilesOverlay.LoadingBackgroundColor = Color.Transparent;
-                mapView.OverlayManager.Add(tilesOverlay);
+                mapView.OverlayManager.Add (tilesOverlay);
                 mapView.SetMultiTouchControls (true);
                 mapView.TilesScaledToDpi = true;
 
@@ -317,14 +319,14 @@ namespace de.upb.hip.mobile.droid.Map {
             mapView.Invalidate ();
         }
 
-        private void DrawRoute(OrderedRoute route, bool userLocationIncluded)
+        private void DrawRoute (OrderedRoute route, bool userLocationIncluded)
         {
             if (disposed)
                 return;
 
             //Cleanup route if drawn before
             if (currentRouteOverlay != null)
-                mapView.OverlayManager.Remove(currentRouteOverlay);
+                mapView.OverlayManager.Remove (currentRouteOverlay);
             if (currentSectionOverlay != null)
             {
                 mapView.OverlayManager.Remove (currentSectionOverlay);
@@ -354,12 +356,14 @@ namespace de.upb.hip.mobile.droid.Map {
             }
             else
             {
-                currentRouteOverlay = new Polyline(activity)
+                currentRouteOverlay = new Polyline (activity)
                 {
                     Title = osmMap.DetailsRoute.Title,
                     Width = 5f,
+
                     Color = ((Xamarin.Forms.Color)resources.GetResourceValue("PrimaryColor")).ToAndroid(),
                     Points = route.Locations.Select(geoLocation => new GeoPoint(geoLocation.Latitude, geoLocation.Longitude)).ToList(),
+
                     Geodesic = true
                 };
             }
@@ -368,8 +372,8 @@ namespace de.upb.hip.mobile.droid.Map {
             {
                 mapView.OverlayManager.Add (index, currentSectionOverlay);
             }
-            mapView.OverlayManager.Add(index, currentRouteOverlay);
-            mapView.Invalidate();
+            mapView.OverlayManager.Add (index, currentRouteOverlay);
+            mapView.Invalidate ();
         }
 
         private bool disposed;
