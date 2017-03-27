e9396d9622978c73ce0914a5ae9be57dabd1a4e2
Bastian Brinkmann <babri@mail.uni-paderborn.de>
2017-03-27 10:11:51 +0200

    Added tests for changing the color

--- Sources/HipMobileUI.Tests/ViewModels/Pages/ExhibitDetailsViewModelTest.cs
+++ Sources/HipMobileUI.Tests/ViewModels/Pages/ExhibitDetailsViewModelTest.cs
@@ -39,8 +39,7 @@ namespace HipMobileUI.Tests.ViewModels.Pages
             IoCManager.RegisterInstance(typeof(INavigationService), Substitute.For<INavigationService>());
             IoCManager.RegisterInstance(typeof(IImageDimension), Substitute.For<IImageDimension>());
             IoCManager.RegisterInstance(typeof(IAudioPlayer), Substitute.For<IAudioPlayer>());
-            IoCManager.RegisterInstance (typeof(IBarsColorsChanger), Substitute.For<IBarsColorsChanger> ());
-            IoCManager.RegisterInstance (typeof (ApplicationResourcesProvider), new ApplicationResourcesProvider (
+            IoCManager.RegisterInstance(typeof(ApplicationResourcesProvider), new ApplicationResourcesProvider(
                                              new Dictionary<string, object>
                                              {
                                                  {"PrimaryDarkColor", Color.Pink},
@@ -51,6 +50,7 @@ namespace HipMobileUI.Tests.ViewModels.Pages
         [Test, Category("UnitTest")]
         public void NextView_Single()
         {
+            IoCManager.RegisterInstance(typeof(IBarsColorsChanger), Substitute.For<IBarsColorsChanger>());
             var sut = CreateSystemUnderTest();
 
             var selectedViewOne = sut.SelectedView;
@@ -61,31 +61,33 @@ namespace HipMobileUI.Tests.ViewModels.Pages
         }
 
         [Test, Category("UnitTest")]
-        public void PreviousView_Single ()
+        public void PreviousView_Single()
         {
-            var sut = CreateSystemUnderTest ();
-            sut.NextViewCommand.Execute (null);
+            IoCManager.RegisterInstance(typeof(IBarsColorsChanger), Substitute.For<IBarsColorsChanger>());
+            var sut = CreateSystemUnderTest();
+            sut.NextViewCommand.Execute(null);
 
             var selectedViewOne = sut.SelectedView;
-            sut.PreviousViewCommand.Execute (null);
+            sut.PreviousViewCommand.Execute(null);
             var selectedViewTwo = sut.SelectedView;
 
-            Assert.AreNotEqual (selectedViewOne, selectedViewTwo);
+            Assert.AreNotEqual(selectedViewOne, selectedViewTwo);
         }
 
         [Test, Category("UnitTest")]
         public void Navigation_All()
         {
+            IoCManager.RegisterInstance(typeof(IBarsColorsChanger), Substitute.For<IBarsColorsChanger>());
             var sut = CreateSystemUnderTest();
 
             // forwards navigation
             int navigations = 0;
             while (sut.NextViewAvailable)
             {
-                sut.NextViewCommand.Execute (null);
+                sut.NextViewCommand.Execute(null);
                 navigations++;
             }
-            Assert.AreEqual (navigations, 2);
+            Assert.AreEqual(navigations, 2);
 
             // backward navigation
             while (sut.PreviousViewAvailable)
@@ -96,25 +98,27 @@ namespace HipMobileUI.Tests.ViewModels.Pages
             Assert.AreEqual(navigations, 0);
         }
 
-        [Test, Category ("UnitTest")]
-        public void Creation_PropertiesFilled ()
+        [Test, Category("UnitTest")]
+        public void Creation_PropertiesFilled()
         {
-            var sut = CreateSystemUnderTest ();
+            IoCManager.RegisterInstance(typeof(IBarsColorsChanger), Substitute.For<IBarsColorsChanger>());
+            var sut = CreateSystemUnderTest();
 
-            Assert.AreEqual (sut.PreviousViewAvailable, false);
+            Assert.AreEqual(sut.PreviousViewAvailable, false);
             Assert.AreEqual(sut.NextViewAvailable, true);
-            Assert.NotNull (sut.SelectedView);
-            Assert.NotNull (sut.NextViewCommand);
+            Assert.NotNull(sut.SelectedView);
+            Assert.NotNull(sut.NextViewCommand);
             Assert.NotNull(sut.PreviousViewCommand);
         }
 
         [Test, Category("UnitTest")]
-        public void PageToViewModel_All ()
+        public void PageToViewModel_All()
         {
-            var sut = CreateSystemUnderTest ();
+            IoCManager.RegisterInstance(typeof(IBarsColorsChanger), Substitute.For<IBarsColorsChanger>());
+            var sut = CreateSystemUnderTest();
 
-            Assert.IsInstanceOf<AppetizerViewModel> (sut.SelectedView);
-            sut.NextViewCommand.Execute (null);
+            Assert.IsInstanceOf<AppetizerViewModel>(sut.SelectedView);
+            sut.NextViewCommand.Execute(null);
             Assert.IsInstanceOf<ImageViewModel>(sut.SelectedView);
             sut.NextViewCommand.Execute(null);
             Assert.IsInstanceOf<TimeSliderViewModel>(sut.SelectedView);
@@ -123,8 +127,9 @@ namespace HipMobileUI.Tests.ViewModels.Pages
         [Test, Category("UnitTest")]
         public void ToggleButtonVisibility_IsVisibleChangedAfter3Secs()
         {
-            var sut = CreateSystemUnderTest ();
-            sut.NextViewCommand.Execute (null);
+            IoCManager.RegisterInstance(typeof(IBarsColorsChanger), Substitute.For<IBarsColorsChanger>());
+            var sut = CreateSystemUnderTest();
+            sut.NextViewCommand.Execute(null);
 
             Assert.AreEqual(true, sut.NextVisible);
             Assert.AreEqual(true, sut.PreviousVisible);
@@ -135,29 +140,49 @@ namespace HipMobileUI.Tests.ViewModels.Pages
             Assert.AreEqual(false, sut.PreviousVisible);
         }
 
+        [Test, Category("UnitTest")]
+        public void Creation_AdditionalInformationWithCorrectStatusBarColors()
+        {
+            var resources = Substitute.For<IBarsColorsChanger> ();
+            IoCManager.RegisterInstance(typeof(IBarsColorsChanger), resources);
+            var sut = new ExhibitDetailsViewModel(new List<Page>(), "Test", true);
+
+            resources.Received().ChangeToolbarColor(Color.FromRgb(128, 128, 128), Color.FromRgb(169, 169, 169));
+        }
+
+        [Test, Category("UnitTest")]
+        public void Creation_NormalStatusBarColors()
+        {
+            var resources = Substitute.For<IBarsColorsChanger>();
+            IoCManager.RegisterInstance(typeof(IBarsColorsChanger), resources);
+            var sut = new ExhibitDetailsViewModel (new List<Page> (), "Test");
+
+            resources.Received().ChangeToolbarColor(Color.Pink, Color.Pink);
+        }
+
         #region Helper Methods
 
         public ExhibitDetailsViewModel CreateSystemUnderTest()
         {
-            var exhibit = Substitute.For<Exhibit> ();
-            var pages = new List<Page> {CreateAppetizerPage (), CreateImagePage (), CreateTimeSliderPage ()};
-            exhibit.Pages.Returns (pages);
-            
-            return new ExhibitDetailsViewModel (exhibit);
+            var exhibit = Substitute.For<Exhibit>();
+            var pages = new List<Page> { CreateAppetizerPage(), CreateImagePage(), CreateTimeSliderPage() };
+            exhibit.Pages.Returns(pages);
+
+            return new ExhibitDetailsViewModel(exhibit);
         }
 
-        private Page CreateAppetizerPage ()
+        private Page CreateAppetizerPage()
         {
-            var page = Substitute.For<Page> ();
-            page.AppetizerPage = Substitute.For<AppetizerPage> ();
-            page.AppetizerPage.Image = CreateImage ();
+            var page = Substitute.For<Page>();
+            page.AppetizerPage = Substitute.For<AppetizerPage>();
+            page.AppetizerPage.Image = CreateImage();
             return page;
         }
         private Page CreateImagePage()
         {
             var page = Substitute.For<Page>();
             page.ImagePage = Substitute.For<ImagePage>();
-            page.ImagePage.Image = CreateImage ();
+            page.ImagePage.Image = CreateImage();
             return page;
         }
 
@@ -168,7 +193,7 @@ namespace HipMobileUI.Tests.ViewModels.Pages
             return page;
         }
 
-        private Image CreateImage ()
+        private Image CreateImage()
         {
             var image = Substitute.For<Image>();
             image.Data = new byte[] { 1, 2, 3, 4 };
