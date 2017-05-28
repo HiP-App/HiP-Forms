119467f4c64a1ca829a07476f181ba88abf7c080
Rico Leder <lederr@mail.uni-paderborn.de>
2017-05-28 20:50:18 +0200

    Improved needed data calculation + added fetched media to pages

--- Sources/HiPMobile/BusinessLayer/ContentApiFetchers/FullExhibitDataFetcher.cs
+++ Sources/HiPMobile/BusinessLayer/ContentApiFetchers/FullExhibitDataFetcher.cs
@@ -13,12 +13,14 @@
 // limitations under the License.
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Practices.Unity;
 using PaderbornUniversity.SILab.Hip.Mobile.Shared.BusinessLayer.ContentApiFetchers.Contracts;
 using PaderbornUniversity.SILab.Hip.Mobile.Shared.BusinessLayer.DtoToModelConverters;
 using PaderbornUniversity.SILab.Hip.Mobile.Shared.BusinessLayer.Managers;
+using PaderbornUniversity.SILab.Hip.Mobile.Shared.BusinessLayer.Models;
 using PaderbornUniversity.SILab.Hip.Mobile.Shared.Helpers;
 using PaderbornUniversity.SILab.Hip.Mobile.Shared.ServiceAccessLayer.ContentApiAccesses.Contracts;
 using PaderbornUniversity.SILab.Hip.Mobile.Shared.ServiceAccessLayer.ContentApiDtos;
@@ -39,15 +41,19 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.Shared.BusinessLayer.ContentApiFe
         }
 
         private int idForRestApi;
-        private IList<int?> requiredPageImages;
+        private IList<int?> requiredPages;
+        private IList<int?> requiredImages;
+        private IList<int?> requiredAudios;
         private IList<PageDto> pageItems;
 
         public async Task FetchFullExhibitDataIntoDatabase (int idForRestApi, CancellationToken token, IProgressListener listener)
         {
             this.idForRestApi = idForRestApi;
-            requiredPageImages = new List<int?> ();
+            requiredPages = new List<int?> ();
+            requiredImages = new List<int?>();
+            requiredAudios = new List<int?>();
 
-            double totalSteps = await FetchNeededDataForFullExhibit();
+            double totalSteps = await FetchNeededPagesForFullExhibit();
 
             if (token.IsCancellationRequested)
             {
@@ -66,19 +72,29 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.Shared.BusinessLayer.ContentApiFe
             }
         }
 
-        private async Task<int> FetchNeededDataForFullExhibit()
+        private async Task<int> FetchNeededPagesForFullExhibit()
         {
             pageItems = (await pagesApiAccess.GetPages(idForRestApi)).Items;
             foreach (var page in pageItems)
             {
-                requiredPageImages.Add (page.Image);
+                requiredPages.Add (page.Id);
+                if (page.Image.HasValue)
+                {
+                    requiredImages.Add (page.Image);
+                }
+                if (page.Audio.HasValue)
+                {
+                    requiredAudios.Add (page.Audio);
+                }
             }
-            return requiredPageImages.Count;
+            return requiredPages.Count + requiredImages.Count + requiredAudios.Count;
         }
 
         private async Task ProcessPages(CancellationToken token, IProgressListener listener)
         {
-            var fetchedMedia = await mediaDataFetcher.FetchMedias(requiredPageImages, token, listener);
+            var fetchedMediaImages = await mediaDataFetcher.FetchMedias(requiredImages, token, listener);
+            var fetchedMediaAudios = await mediaDataFetcher.FetchMedias(requiredAudios, token, listener);
+
             if (token.IsCancellationRequested)
             {
                 return;
@@ -88,9 +104,55 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.Shared.BusinessLayer.ContentApiFe
             {
                 var dbPage = PageConverter.Convert(pageDto);
 
-                //AddImageToPage(dbPage, pageDto.Image, fetchedMedia); //TODO: Implementation
+                AddContentToPage(dbPage, pageDto, fetchedMediaImages, fetchedMediaAudios);
+                //AddPageToExhibit(pageDto, dbPage, fetchedMediaImages, fetchedMediaAudios); TODO: implementation
 
                 listener.ProgressOneStep();
+            }
+        }
+
+        private void AddContentToPage(Page dbPage, PageDto content, FetchedMediaData fetchedMediaImages, FetchedMediaData fetchedMediaAudios)
+        {
+            switch (dbPage.PageType)
+            {
+                case PageType.AppetizerPage:
+                    break;
+                case PageType.ImagePage:
+                    var image = fetchedMediaImages.Images.SingleOrDefault(x => x.IdForRestApi == content.Image);
+                
+                    if (image != null)
+                    {
+                        dbPage.ImagePage.Image = image;
+                    }
+                    else
+                    {
+                        dbPage.ImagePage.Image = BackupData.BackupImage;
+                    }
+                    break;
+                case PageType.TextPage:
+                    break;
+                case PageType.TimeSliderPage:
+                    var images = fetchedMediaImages.Images;
+
+                    if (images.Count > 0)
+                    {
+                        foreach (var img in images)
+                        {
+                            dbPage.TimeSliderPage.Images.Add (img);
+                        }
+                    }
+                    else
+                    {
+                        dbPage.TimeSliderPage.Images.Add(BackupData.BackupImage);
+                    }
+                    break;
+            }
+
+            var audio = fetchedMediaAudios.Audios.SingleOrDefault(x => x.IdForRestApi == content.Audio);
+
+            if (audio != null)
+            {
+                dbPage.Audio = audio;
             }
         }
     }
