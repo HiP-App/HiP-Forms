441619143417605da65dd7128d657132f45922f8
Mklemens <mklemens@campus.uni-paderborn.de>
2017-05-29 15:22:19 +0200

    Account view can be displayed

---
+++ Sources/HipMobileUI/Pages/ProfilePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PaderbornUniversity.SILab.Hip.Mobile.UI.Navigation;
+using PaderbornUniversity.SILab.Hip.Mobile.UI.ViewModels.Pages;
+using Xamarin.Forms.Xaml;
+
+namespace PaderbornUniversity.SILab.Hip.Mobile.UI.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : IViewFor<ProfilePageViewModel>
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Sources/HipMobileUI/ViewModels/Views/AccountScreenViewModel.cs
+++ Sources/HipMobileUI/ViewModels/Views/AccountScreenViewModel.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PaderbornUniversity.SILab.Hip.Mobile.Shared.Helpers;
+using PaderbornUniversity.SILab.Hip.Mobile.UI.ViewModels.Pages;
 
 namespace PaderbornUniversity.SILab.Hip.Mobile.UI.ViewModels.Views
 {
     public class AccountScreenViewModel : NavigationViewModel
     {
+
+        public AccountScreenViewModel ()
+        {
+            Settings.IsLoggedIn = false;
+            Settings.IsLoggedIn = true;
+            if (Settings.IsLoggedIn)
+            {
+                SwitchToProfileScreen ();
+            }
+            else
+            {
+                // Insert your registration / login code here
+            }
+
+        }
+
+        public async void SwitchToProfileScreen ()
+        {
+            await Navigation.PushModalAsync (new ProfilePageViewModel ());
+        }
     }
+
 }
