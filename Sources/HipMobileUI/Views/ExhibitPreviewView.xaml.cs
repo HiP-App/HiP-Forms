8d69961d3a5d7828bf7876f464749c72e0118b7a
MKlemens <mklemens@campus.uni-paderborn.de>
2017-09-12 09:48:55 +0200

    Improved responsive dialogue areas on pop-ups (#111)

--- Sources/HipMobileUI/Views/ExhibitPreviewView.xaml.cs
+++ Sources/HipMobileUI/Views/ExhibitPreviewView.xaml.cs
@@ -39,15 +39,19 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.UI.Views
                 PageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.35, GridUnitType.Star) });
                 PageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.15, GridUnitType.Star) });
 
-                PageGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                PageGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(width / 2) });
+                PageGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(width / 2) });
 
                 Grid.SetRow(ImageView, 0);
                 Grid.SetRow(QuestionView, 1);
                 Grid.SetRow(ButtonView, 2);
 
                 Grid.SetColumn(ImageView, 0);
+                Grid.SetColumnSpan (ImageView, 2);
                 Grid.SetColumn(QuestionView, 0);
+                Grid.SetColumnSpan(QuestionView, 2);
                 Grid.SetColumn(ButtonView, 0);
+                Grid.SetColumnSpan(ButtonView, 2);
 
                 QuestionView.Padding = new Thickness(10, 0, 10, 0);
                 ImageView.Aspect = Aspect.AspectFill;
@@ -62,15 +66,19 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.UI.Views
                 PageGrid.RowDefinitions.Clear();
                 PageGrid.ColumnDefinitions.Clear();
 
-                PageGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                PageGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(width / 2) });
+                PageGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(width / 2) });
 
                 PageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.6, GridUnitType.Star) });
                 PageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.2, GridUnitType.Star) });
                 PageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.2, GridUnitType.Star) });
 
                 Grid.SetColumn(ImageView, 0);
+                Grid.SetColumnSpan(ImageView, 2);
                 Grid.SetColumn(QuestionView, 0);
+                Grid.SetColumnSpan(QuestionView, 2);
                 Grid.SetColumn(ButtonView, 0);
+                Grid.SetColumnSpan(ButtonView, 2);
 
                 Grid.SetRow(ImageView, 0);
                 Grid.SetRow(QuestionView, 1);
