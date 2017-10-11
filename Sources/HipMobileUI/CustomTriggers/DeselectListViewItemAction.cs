ca6addd9155048710fa199c9bb69945a49530016
cbruegg <mail@cbruegg.com>
2017-10-11 19:58:02 +0100

    Remove ugly ListView cell highlight from achievements list

---
+++ Sources/HipMobileUI/CustomTriggers/DeselectListViewItemAction.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (C) 2017 History in Paderborn App - Universität Paderborn
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Xamarin.Forms;
+
+namespace PaderbornUniversity.SILab.Hip.Mobile.UI.CustomTriggers
+{
+    /// <summary>
+    /// Useful for making the ugly selection highlight color disappear.
+    /// See also https://stackoverflow.com/a/42572336/1502352.
+    /// </summary>
+    public class DeselectListViewItemAction : TriggerAction<ListView>
+    {
+        protected override void Invoke(ListView sender)
+        {
+            sender.SelectedItem = null;
+        }
+    }
+}
