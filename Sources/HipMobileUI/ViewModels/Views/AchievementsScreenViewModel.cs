01326c901aa1be03ff3fd02b14e1146b03472f36
Sayanti Kundu <sayantik@mail.uni-paderborn.de>
2017-10-30 21:26:30 +0100

    changes as per review comments

--- Sources/HipMobileUI/ViewModels/Views/AchievementsScreenViewModel.cs
+++ Sources/HipMobileUI/ViewModels/Views/AchievementsScreenViewModel.cs
@@ -8,55 +8,43 @@ using Xamarin.Forms;
 
 namespace PaderbornUniversity.SILab.Hip.Mobile.UI.ViewModels.Views
 {
-    public class AchievementsScreenViewModel : NavigationViewModel
-    {
-        private readonly MainPageViewModel mainPageViewModel;
-
-public AchievementsScreenViewModel()
-        {
-			//mainPageViewModel = mainPageVm;
+	public class AchievementsScreenViewModel : NavigationViewModel
+	{
+		public AchievementsScreenViewModel()
+		{
 			//need to make changes so that achievements screen asks user to log in when no user logged in
-			//if (Settings.IsLoggedIn == false)
-				
+			Achievements = new ObservableCollection<string>
+			{
+				"Achievement 1",
+				"Achievement 2",
+				"Achievement 3",
+				"Achievement 4",
+				"Achievement 5",
+				"Achievement 6",
+				"Achievement 7"
+			};
 
-            
+		}
 
-            Achievements = new ObservableCollection<string>
-            {
-                "Achievement 1",
-                "Achievement 2",
-                "Achievement 3",
-                "Achievement 4",
-                "Achievement 5",
-                "Achievement 6",
-                "Achievement 7"
-            };
+		public string Username => Settings.Username;
+		public int Score => Settings.Score;
+		public string Completeness => Settings.Completeness + "%";
 
-            
-        }
+		private ObservableCollection<String> achievements;
 
+		public ObservableCollection<String> Achievements
+		{
+			get { return achievements; }
+			set { SetProperty(ref achievements, value); }
+		}
 
-        
-        public String Username => Settings.Username;
-        public int Score => Settings.Score;
-        public String Completeness => Settings.Completeness + "%";
+		private ObservableCollection<string> tabs;
 
+		public ObservableCollection<string> Tabs
+		{
+			get { return tabs; }
+			set { SetProperty(ref tabs, value); }
+		}
 
-        private ObservableCollection<String> achievements;
-
-        public ObservableCollection<String> Achievements
-        {
-            get { return achievements; }
-            set { SetProperty(ref achievements, value); }
-        }
-
-        private ObservableCollection<string> tabs;
-
-        public ObservableCollection<string> Tabs
-        {
-            get { return tabs; }
-            set { SetProperty(ref tabs, value); }
-        }
- 
-    }
+	}
 }
--- Sources/HipMobileUI/Views/AchievementsScreenView.xaml.cs
+++ Sources/HipMobileUI/Views/AchievementsScreenView.xaml.cs
@@ -7,70 +7,43 @@ using Xamarin.Forms.Xaml;
 
 namespace PaderbornUniversity.SILab.Hip.Mobile.UI.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AchievementsScreenView : IViewFor<AchievementsScreenViewModel>
-    {
-        private double thisWidth, thisHeight;
-        private DeviceOrientation deviceOrientation;
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AchievementsScreenView : IViewFor<AchievementsScreenViewModel>
+	{
+		private double thisWidth, thisHeight;
+		private DeviceOrientation deviceOrientation;
 
-        public AchievementsScreenView()
-        {
-            InitializeComponent();
-            deviceOrientation = DeviceOrientation.Undefined;
-        }
+		public AchievementsScreenView()
+		{
+			InitializeComponent();
+			deviceOrientation = DeviceOrientation.Undefined;
+		}
+		protected override void OnSizeAllocated(double width, double height)
+		{
+			base.OnSizeAllocated(width, height);
 
-        protected override void OnSizeAllocated(double width, double height)
-        {
-            base.OnSizeAllocated(width, height);
+			if (!(Math.Abs(width - thisWidth) > 0.4) && !(Math.Abs(height - thisHeight) > 0.4))
+				return;
 
-            if (!(Math.Abs(width - thisWidth) > 0.4) && !(Math.Abs(height - thisHeight) > 0.4))
-                return;
+			thisWidth = width;
+			thisHeight = height;
 
-            thisWidth = width;
-            thisHeight = height;
+			if (width <= height)
+			{
+				// Orientation changes to Portrait
+				if (deviceOrientation == DeviceOrientation.Portrait)
+					return;
 
-            if (width <= height)
-            {
-                // Orientation changes to Portrait
-                if (deviceOrientation == DeviceOrientation.Portrait)
-                    return;
+				deviceOrientation = DeviceOrientation.Portrait;
+			}
+			else if (width > height)
+			{
+				// Orientation changes to Landscape
+				if (deviceOrientation == DeviceOrientation.Landscape)
+					return;
 
-                // Overview
-                //OverviewGrid.RowDefinitions.Clear();
-                //OverviewGrid.ColumnDefinitions.Clear();
-
-                //OverviewGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.4, GridUnitType.Star) });
-                //OverviewGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.4, GridUnitType.Star) });
-                //OverviewGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.2, GridUnitType.Star) });
-
-                //OverviewGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-
-
-                // Achievements
-
-                deviceOrientation = DeviceOrientation.Portrait;
-            }
-            else if (width > height)
-            {
-                // Orientation changes to Landscape
-                if (deviceOrientation == DeviceOrientation.Landscape)
-                    return;
-
-                // Overview
-                //OverviewGrid.RowDefinitions.Clear();
-                //OverviewGrid.ColumnDefinitions.Clear();
-
-                //OverviewGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(0.5, GridUnitType.Star) });
-                //OverviewGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(0.5, GridUnitType.Star) });
-
-                //OverviewGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.6, GridUnitType.Star) });
-                //OverviewGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.4, GridUnitType.Star) });
-
-
-                // Achievements
-
-                deviceOrientation = DeviceOrientation.Landscape;
-            }
-        }
-    }
+				deviceOrientation = DeviceOrientation.Landscape;
+			}
+		}
+	}
 }
