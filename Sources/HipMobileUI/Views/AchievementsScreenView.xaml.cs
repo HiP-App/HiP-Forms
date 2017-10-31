f2a8d4a4b41ded6ee0b2a834c83067e51022f237
cbruegg <mail@cbruegg.com>
2017-10-31 11:45:19 +0100

    Remove unused properties

--- Sources/HipMobileUI/Views/AchievementsScreenView.xaml.cs
+++ Sources/HipMobileUI/Views/AchievementsScreenView.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using PaderbornUniversity.SILab.Hip.Mobile.UI.Helpers;
-using PaderbornUniversity.SILab.Hip.Mobile.UI.Navigation;
+﻿using PaderbornUniversity.SILab.Hip.Mobile.UI.Navigation;
 using PaderbornUniversity.SILab.Hip.Mobile.UI.ViewModels.Views;
 using Xamarin.Forms.Xaml;
 
@@ -9,41 +7,9 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.UI.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AchievementsScreenView : IViewFor<AchievementsScreenViewModel>
     {
-        private double thisWidth, thisHeight;
-        private DeviceOrientation deviceOrientation;
-
         public AchievementsScreenView()
         {
             InitializeComponent();
-            deviceOrientation = DeviceOrientation.Undefined;
-        }
-
-        protected override void OnSizeAllocated(double width, double height)
-        {
-            base.OnSizeAllocated(width, height);
-
-            if (!(Math.Abs(width - thisWidth) > 0.4) && !(Math.Abs(height - thisHeight) > 0.4))
-                return;
-
-            thisWidth = width;
-            thisHeight = height;
-
-            if (width <= height)
-            {
-                // Orientation changes to Portrait
-                if (deviceOrientation == DeviceOrientation.Portrait)
-                    return;
-
-                deviceOrientation = DeviceOrientation.Portrait;
-            }
-            else if (width > height)
-            {
-                // Orientation changes to Landscape
-                if (deviceOrientation == DeviceOrientation.Landscape)
-                    return;
-
-                deviceOrientation = DeviceOrientation.Landscape;
-            }
         }
     }
 }
