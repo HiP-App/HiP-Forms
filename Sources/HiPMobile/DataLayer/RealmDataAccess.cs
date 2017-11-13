24a87d7ca699205f7ebd60b1bc5e42654a3d8dd5
zain89 <zain46@campus.uni-paderborn.de>
2017-11-13 09:17:14 +0100

    Iss hipm 808 (#142)
    * Change Andriod Package Name
    
    * Temp

--- Sources/HiPMobile/DataLayer/RealmDataAccess.cs
+++ Sources/HiPMobile/DataLayer/RealmDataAccess.cs
@@ -37,7 +37,7 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.Shared.DataLayer
         {
             lock (Locker)
             {
-                // Realm has problems when using LINQ expression here
+                // Realm has problems when using LINQ expression here.
                 var objects = Instance.All<T>();
                 foreach (var realmResult in objects)
                 {
