d4dd6137c751523dd3aac9b4ea7209d88cf7f8bd
Christian Brüggemann <mail@cbruegg.com>
2017-12-27 20:14:06 +0100

    Fix build

--- Sources/HipMobileUI/Resources/Strings.Designer.cs
+++ Sources/HipMobileUI/Resources/Strings.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace PaderbornUniversity.SILab.Hip.Mobile.UI.Resources {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -19,7 +20,7 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.UI.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Strings {
@@ -39,7 +40,7 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.UI.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PaderbornUniversity.SILab.Hip.Mobile.UI.Resources.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PaderbornUniversity.SILab.Hip.Mobile.UI.Resources.Strings", typeof(Strings).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
