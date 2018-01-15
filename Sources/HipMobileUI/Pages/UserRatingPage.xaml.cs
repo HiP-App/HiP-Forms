f17c06a0677125e62f9d838da06bd341c0318e33
DANIEL\Daniel <Daniel.Possienke@googlemail.com>
2018-01-15 22:40:38 +0100

    Added horizontal layout for the user rating page.

--- Sources/HipMobileUI/Pages/UserRatingPage.xaml.cs
+++ Sources/HipMobileUI/Pages/UserRatingPage.xaml.cs
@@ -14,14 +14,61 @@
 
 using PaderbornUniversity.SILab.Hip.Mobile.UI.Navigation;
 using PaderbornUniversity.SILab.Hip.Mobile.UI.ViewModels.Pages;
+using Xamarin.Forms;
 
 namespace PaderbornUniversity.SILab.Hip.Mobile.UI.Pages
 {
     public partial class UserRatingPage : IViewFor<UserRatingPageViewModel>
     {
+        private double width;
+        private double height;
+
         public UserRatingPage()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Size changed, determine if we need to update the layout.
+        /// </summary>
+        /// <param name="width">The new width.</param>
+        /// <param name="height">The new height.</param>
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            if (width != this.width || height != this.height)
+            {
+                this.width = width;
+                this.height = height;
+                if (width > height)
+                {
+                    OuterAbsoluteLayout.Children.Remove(Image);
+                    OuterAbsoluteLayout.Children.Add(Image, new Rectangle(0, 0, 0.5, 1), AbsoluteLayoutFlags.All);
+                    OuterGrid.RowDefinitions.Clear();
+                    OuterGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    OuterGrid.ColumnDefinitions.Clear();
+                    OuterGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(0.5, GridUnitType.Star) });
+                    OuterGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(0.5, GridUnitType.Star) });
+                    OuterGrid.Children.Remove(InnerGrid1);
+                    OuterGrid.Children.Add(InnerGrid1, 0, 0);
+                    OuterGrid.Children.Remove(InnerGrid2);
+                    OuterGrid.Children.Add(InnerGrid2, 1, 0);
+                }
+                else
+                {
+                    OuterAbsoluteLayout.Children.Remove(Image);
+                    OuterAbsoluteLayout.Children.Add(Image,new Rectangle(0, 0, 1, 0.6),AbsoluteLayoutFlags.All);
+                    OuterGrid.ColumnDefinitions.Clear();
+                    OuterGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                    OuterGrid.RowDefinitions.Clear();
+                    OuterGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.6, GridUnitType.Star) });
+                    OuterGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(0.4, GridUnitType.Star) });
+                    OuterGrid.Children.Remove(InnerGrid1);
+                    OuterGrid.Children.Add(InnerGrid1, 0, 0);
+                    OuterGrid.Children.Remove(InnerGrid2);
+                    OuterGrid.Children.Add(InnerGrid2, 0, 1);
+                }
+            }
         }
     }
 }
