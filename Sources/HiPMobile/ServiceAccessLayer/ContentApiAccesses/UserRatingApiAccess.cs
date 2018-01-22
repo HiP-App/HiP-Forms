b3e5328563d182209d6205a0114706031fca22ef
Daniel Possienke <Daniel.Possienke@googlemail.com>
2018-01-23 00:20:25 +0100

    Added white background to the user rating page.

--- Sources/HiPMobile/ServiceAccessLayer/ContentApiAccesses/UserRatingApiAccess.cs
+++ Sources/HiPMobile/ServiceAccessLayer/ContentApiAccesses/UserRatingApiAccess.cs
@@ -41,8 +41,8 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.Shared.ServiceAccessLayer.Content
 
         public async Task<int> GetPreviousUserRatingAsync(int idForRestApi)
         {
-            var url = RequestPath + idForRestApi;
-            var json = await contentApiClient.GetResponseFromUrlAsString(url);
+            //var url = RequestPath + "My/" + idForRestApi;
+            //var json = await contentApiClient.GetResponseFromUrlAsString(url);
             return 0;
         }
 
