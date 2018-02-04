058e96c4c60f927092a4a57704b58cced65177dc
cbruegg <mail@cbruegg.com>
2018-02-04 16:57:16 +0100

    Fix namespaces

--- Sources/HiPMobile/ServiceAccessLayer/AuthenticationApiAccess/AuthApiDto/Token.cs
+++ Sources/HiPMobile/ServiceAccessLayer/AuthenticationApiAccess/AuthApiDto/Token.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
-namespace PaderbornUniversity.SILab.Hip.Mobile.Shared.ServiceAccessLayer.AuthApiAccess.AuthApiDto
+namespace PaderbornUniversity.SILab.Hip.Mobile.Shared.ServiceAccessLayer.AuthenticationApiAccess.AuthApiDto
     {
 
     public class Token
