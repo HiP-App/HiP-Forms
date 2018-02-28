80982278fdac17e73198c74f10f82683be6fab58
Senn3 <Daniel.Possienke@googlemail.com>
2018-02-28 16:12:31 +0100

    Iss hipm 910 As a user, I want to use the application in English (#195)
    * Added scripts for the string translation
    
    * Added the first translated texts
    
    * Added some translations for german texts
    
    * Added a translations for the last german strings
    
    * Reformated the Strings.resx
    
    * Fixed some typos
    
    * Changed "Downloading..." to "Downloading additional data"
    
    * Made some changes according to the comments of markus
    
    * Replaced capital letters in title strings with lowercase letters

---
+++ Sources/HipMobileUI.iOS/Helpers/Localize.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+using System.Globalization;
+using PaderbornUniversity.SILab.Hip.Mobile.UI.Location;
+using Foundation;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PaderbornUniversity.SILab.Hip.Mobile.iOS.Helpers.Localize))]
+
+namespace PaderbornUniversity.SILab.Hip.Mobile.iOS.Helpers
+{
+    public class Localize : ILocalize
+    {
+        public void SetLocale(CultureInfo ci)
+        {
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            var netLanguage = "en";
+            var test = NSLocale.PreferredLanguages;
+            if (NSLocale.PreferredLanguages.Length > 0)
+            {
+                netLanguage = NSLocale.PreferredLanguages[0].Replace("_", "-");
+            }
+
+            // this gets called a lot - try/catch can be expensive so consider caching or something
+            CultureInfo ci;
+            try
+            {
+                ci = new CultureInfo(netLanguage);
+            }
+            catch (CultureNotFoundException)
+            {
+                try
+                {
+                    ci = new CultureInfo(netLanguage.Split('-')[0]);
+                }
+                catch (CultureNotFoundException)
+                {
+                    // falling back to English
+                    ci = new CultureInfo("en");
+                }
+            }
+            return ci;
+        }
+    }
+}
