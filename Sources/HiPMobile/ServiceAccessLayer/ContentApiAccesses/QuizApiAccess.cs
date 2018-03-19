97a86757bd923ff88eb94b42b9a4f2786da1168b
smritisrinivas <30934510+smritisrinivas@users.noreply.github.com>
2018-03-19 13:59:28 +0100

    iss-hipm-895-smriti
    created the necessary classes to get the content.

---
+++ Sources/HiPMobile/ServiceAccessLayer/ContentApiAccesses/QuizApiAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PaderbornUniversity.SILab.Hip.Mobile.Shared.ServiceAccessLayer.ContentApiAccesses.Contracts;
+using PaderbornUniversity.SILab.Hip.Mobile.Shared.ServiceAccessLayer.ContentApiDtos;
+
+namespace PaderbornUniversity.SILab.Hip.Mobile.Shared.ServiceAccessLayer.ContentApiAccesses
+{
+    public class QuizApiAccess: IQuizApiAccess
+    {
+        // We need this t download JSONs. So if someone needs a QuizApiAccess, he needs to give us this to 
+        // let us fetch JSONs
+        private IContentApiClient apiClient;
+
+        public QuizApiAccess(IContentApiClient apiClient)
+        {
+            this.apiClient = apiClient;
+        }
+
+        public async Task<List<QuestionDto>> GetQuestionsForExhibit(int exhibitId)
+        {
+            var requestPath = $"/api/Exhibits/{exhibitId}/Questions";
+            var json = await apiClient.GetResponseFromUrlAsString(requestPath);
+            return JsonConvert.DeserializeObject<List<QuestionDto>>(json);
+        }
+    }
+}
---
+++ Sources/HiPMobile/ServiceAccessLayer/ContentApiDtos/QuestionDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PaderbornUniversity.SILab.Hip.Mobile.Shared.ServiceAccessLayer.ContentApiDtos
+{
+    public class QuestionDto
+    {
+        [JsonProperty("status")]
+        public string Status { get; private set; }
+
+        [JsonProperty("timestamp")]
+        public DateTimeOffset Timestamp { get; private set; }
+
+        [JsonProperty("text")]
+        public string Text { get; private set; }
+
+        [JsonProperty("id")]
+        public int Id { get; private set; }
+
+        [JsonProperty("exhibitId")]
+        public int ExhibitId { get; private set; }
+
+        [JsonProperty("options")]
+        public string[] Options { get; private set; }
+
+        [JsonProperty("image")]
+        public int Image { get; private set; }
+
+    }
+}
