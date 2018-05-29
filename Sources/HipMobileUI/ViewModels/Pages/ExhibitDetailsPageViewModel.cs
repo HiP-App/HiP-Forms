4ea7cfd8a39bb69954b41b15fb3b036d7e97c5cc
SamanPaderborn <17.saman@gmail.com>
2018-05-29 13:07:48 +0200

    iss-hipm-960
    Both the pop up works

--- Sources/HipMobileUI/ViewModels/Pages/ExhibitDetailsPageViewModel.cs
+++ Sources/HipMobileUI/ViewModels/Pages/ExhibitDetailsPageViewModel.cs
@@ -151,17 +151,6 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.UI.ViewModels.Pages
         /// </summary>
         private async void AudioPlayerOnAudioCompleted()
         {
-            if (Settings.RepeatHintAutoPageSwitch)
-            {
-                // ask for preferred setting regarind automatic page switch
-                Settings.RepeatHintAutoPageSwitch = false;
-                var result = await Navigation.DisplayAlert(Strings.ExhibitDetailsPage_Hinweis,
-                                                           Strings.ExhibitDetailsPage_PageSwitch,
-                                                           Strings.ExhibitDetailsPage_AgreeFeature, Strings.ExhibitDetailsPage_DisagreeFeature).ConfigureAwait(true);
-                Settings.AutoSwitchPage = result;
- 
-            }
-
             // aply automatic page switch if wanted and if the next page isn't the rating page
             if (Settings.AutoSwitchPage && NextViewAvailable && currentViewIndex < pages.Count - 1)
             {
@@ -304,6 +293,17 @@ namespace PaderbornUniversity.SILab.Hip.Mobile.UI.ViewModels.Pages
                                                                Strings.ExhibitDetailsPage_AgreeFeature, Strings.ExhibitDetailsPage_DisagreeFeature);
                     Settings.AutoStartAudio = result;
                     Settings.RepeatHintAudio = false;
+
+                    if (Settings.RepeatHintAutoPageSwitch == result)
+                    {
+                        // ask for preferred setting regarind automatic page switch
+                        Settings.RepeatHintAutoPageSwitch = false;
+                        var result1 = await Navigation.DisplayAlert(Strings.ExhibitDetailsPage_Hinweis,
+                                                                   Strings.ExhibitDetailsPage_PageSwitch,
+                                                                   Strings.ExhibitDetailsPage_AgreeFeature, Strings.ExhibitDetailsPage_DisagreeFeature).ConfigureAwait(true);
+                        Settings.AutoSwitchPage = result1;
+
+                    }
                 }
 
                 //play automatic audio, if wanted
