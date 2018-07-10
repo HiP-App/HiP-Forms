9968c5ffff59236ef462bd3d2cdc2bdee2a5a9f5
nataliefriese <friese@mail.upb.de>
2018-07-10 13:59:23 +0200

    removed unused directives

--- Sources/HipMobileUI/Views/ExhibitsOverviewView.xaml.cs
+++ Sources/HipMobileUI/Views/ExhibitsOverviewView.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PaderbornUniversity.SILab.Hip.Mobile.Shared.Common;
-using PaderbornUniversity.SILab.Hip.Mobile.UI.Appearance;
-using PaderbornUniversity.SILab.Hip.Mobile.UI.Helpers;
-using PaderbornUniversity.SILab.Hip.Mobile.UI.Navigation;
+﻿using PaderbornUniversity.SILab.Hip.Mobile.UI.Navigation;
 using PaderbornUniversity.SILab.Hip.Mobile.UI.ViewModels.Views;
-using Xamarin.Forms;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+
 
 namespace PaderbornUniversity.SILab.Hip.Mobile.UI.Views
 {
