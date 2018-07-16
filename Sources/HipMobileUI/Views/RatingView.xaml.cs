8553a6ac4358473a366ecf27f1ad718482f1bb06
Natalie <32631323+nataliefriese@users.noreply.github.com>
2018-07-16 12:42:37 +0200

    Extract RatingView component (#251)
    * Extraxted RatingView from Appetizer Page
    
    * Extracted Average Rating from UserRatingPage
    
    * removed contentview

---
+++ Sources/HipMobileUI/Views/RatingView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using PaderbornUniversity.SILab.Hip.Mobile.UI.Navigation;
+using PaderbornUniversity.SILab.Hip.Mobile.UI.ViewModels.Views;
+
+
+namespace PaderbornUniversity.SILab.Hip.Mobile.UI.Views
+{
+    public partial class RatingView : IViewFor<RatingViewModel>
+    {
+        public RatingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
