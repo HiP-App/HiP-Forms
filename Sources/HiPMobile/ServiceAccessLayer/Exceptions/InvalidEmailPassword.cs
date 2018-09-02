5f14423bc0d4da720f67809be5946826c795f5c1
ritika1003 <33968830+ritika1003@users.noreply.github.com>
2018-09-02 17:36:16 +0200

    Iss hipm 959 (#259)
    * iss-hipm-959
    
    working on it
    
    * iss-hipm-959
    
    might get changed in the future
    
    * iss-hipm-959
    
    chnages
    
    * iss-hipm-959
    
    username is reflecting on the profile page
    
    * iss-hipm-959
    
    comments removed
    
    * Fixed build error at automated code quality checks

---
+++ Sources/HiPMobile/ServiceAccessLayer/Exceptions/InvalidEmailPassword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaderbornUniversity.SILab.Hip.Mobile.Shared.ServiceAccessLayer.Exceptions
+{
+    class InvalidEmailPassword: Exception 
+    {
+        public InvalidEmailPassword()
+        {
+        }
+
+        public InvalidEmailPassword(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidEmailPassword(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
